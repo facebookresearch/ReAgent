Set of RL improvements: Fix error in quantile computation. Handle missing values in sparse_to_dense. Replace page_size with minibatch size.
Summary: Set of RL improvements: Fix error in quantile computation.  Handle missing values in sparse_to_dense.  Replace page_size with minibatch size.

Differential Revision: D6888977

fbshipit-source-id: bb84477866c64da5ff57d6c25df1c8d3b799e437

ml/rl/preprocessing/identify_types.py
@@ -33,8 +33,9 @@ def _is_continuous(feature_values):
 def _is_enum(feature_values, enum_threshold):
     are_all_ints = np.vectorize(lambda val: float(val).is_integer())
     return (
-        len(np.unique(feature_values)) <= enum_threshold and
-        np.all(are_all_ints(feature_values))
+        np.min(feature_values) >= 0 and  # All values must be positive
+        len(np.unique(feature_values)) <= enum_threshold
+        and np.all(are_all_ints(feature_values))
     )
 
 

ml/rl/preprocessing/normalization.py
@@ -24,7 +24,7 @@ NormalizationParameters = namedtuple(
         'boxcox_shift',
         'mean',
         'stddev',
-        'possible_values',  # Assume present for ENUM type and sorted
+        'possible_values',  # Assume present for ENUM type
         'quantiles',  # Assume present for QUANTILE type and sorted
     ]
 )
@@ -47,7 +47,9 @@ def identify_parameter(
     feature_type=None
 ):
     if feature_type is None:
-        feature_type = identify_types.identify_type(values, max_unique_enum_values)
+        feature_type = identify_types.identify_type(
+            values, max_unique_enum_values
+        )
 
     boxcox_lambda = None
     boxcox_shift = 0
@@ -95,12 +97,18 @@ def identify_parameter(
         if boxcox_lambda is None or skip_box_cox:
             boxcox_shift = None
             boxcox_lambda = None
-        if boxcox_lambda is None and k2_original > quantile_k2_threshold and (not skip_quantiles):
+        if boxcox_lambda is None and k2_original > quantile_k2_threshold and (
+            not skip_quantiles
+        ):
             feature_type = identify_types.QUANTILE
-            quantiles = mquantiles(
-                values,
-                np.arange(quantile_size, dtype=np.float32) /
-                float(quantile_size)
+            quantiles = np.unique(
+                mquantiles(
+                    values,
+                    np.arange(quantile_size + 1, dtype=np.float32) /
+                    float(quantile_size),
+                    alphap=0.0,
+                    betap=1.0,
+                )
             ).astype(float).tolist()
             logger.info(
                 "Feature is non-normal, using quantiles: {}".format(quantiles)
@@ -115,7 +123,7 @@ def identify_parameter(
         values /= stddev
 
     if feature_type == identify_types.ENUM:
-        possible_values = np.unique(values).astype(float).tolist()
+        possible_values = np.unique(values.astype(int)).tolist()
 
     return NormalizationParameters(
         feature_type, boxcox_lambda, boxcox_shift, mean, stddev,
@@ -138,9 +146,19 @@ def get_num_output_features(normalization_parmeters):
 def deserialize(parameters_json):
     parameters = {}
     for feature, feature_parameters in six.iteritems(parameters_json):
-        parameters[feature] = NormalizationParameters(
-            **json.loads(feature_parameters)
-        )
+        params = NormalizationParameters(**json.loads(feature_parameters))
+        # Check for negative enum IDs
+        if params.feature_type == identify_types.ENUM:
+            for x in params.possible_values:
+                if x < 0:
+                    logger.fatal(
+                        "Invalid enum ID: " + str(x) + " in feature: " + feature
+                        + " with possible_values " +
+                        str(params.possible_values) + " (raw: " +
+                        feature_parameters + ")"
+                    )
+                    raise Exception("Invalid enum ID")
+        parameters[feature] = params
     return parameters
 
 

ml/rl/test/test_normalization.py
@@ -27,7 +27,7 @@ class TestNormalization(unittest.TestCase):
         elif right == n_quantiles:
             interpolated = 1.0
         elif quantiles[right] == original_value:
-            interpolated = (right - 1) / float(n_quantiles)
+            interpolated = right / float(n_quantiles)
         else:
             left = right - 1
             step = 1.0 / float(n_quantiles) \
@@ -108,7 +108,8 @@ class TestNormalization(unittest.TestCase):
                 for i, feature in enumerate(v[0]):
                     original_feature = feature_value_map[k][i]
                     expected = self._value_to_quantile(
-                        original_feature, normalization_parameters[k].quantiles)
+                        original_feature, normalization_parameters[k].quantiles
+                    )
                     self.assertAlmostEqual(feature, expected, 2)
             elif feature_type == identify_types.BINARY:
                 pass
@@ -264,8 +265,9 @@ class TestNormalization(unittest.TestCase):
         elif parameters.feature_type == identify_types.QUANTILE:
             transformed_feature = np.zeros_like(feature)
             for i in six.moves.range(feature.shape[0]):
-                transformed_feature[i] = self._value_to_quantile(feature[i],
-                    parameters.quantiles)
+                transformed_feature[i] = self._value_to_quantile(
+                    feature[i], parameters.quantiles
+                )
             feature = transformed_feature
         elif parameters.feature_type == identify_types.ENUM:
             possible_values = parameters.possible_values
@@ -358,7 +360,6 @@ class TestNormalization(unittest.TestCase):
 
         # And ask for a binary anyways
         parameter = normalization.identify_parameter(
-            probability_values,
-            feature_type=identify_types.BINARY
+            probability_values, feature_type=identify_types.BINARY
         )
         self.assertEqual(parameter.feature_type, 'BINARY')