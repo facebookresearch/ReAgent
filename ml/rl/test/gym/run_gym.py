f8880f5581f431fc753f8f8795cd2907f9bbaf39
Saravanakumar Periyaswamy <saravanp@fb.com>
2018-07-05 13:54:55 -0700

    Suppress Caffe2 debug logging in Reinforcement Learning Codebase
    Summary: Caffe2 is emitting lots of debug logs that are hiding logs we care about. This commit updates the Caffe2 logging level using python API to reduce the excessive logging.
    
    Reviewed By: econti
    
    Differential Revision: D8735775
    
    fbshipit-source-id: 841fab4ef26002fc7f70fee6545ab918c9d49caa

--- ml/rl/test/gym/run_gym.py
+++ ml/rl/test/gym/run_gym.py
@@ -290,6 +290,14 @@ def run_gym(
     save_timesteps_to_dataset=None,
     start_saving_from_episode=0,
 ):
+    '''
+    Caffe2 core logging configuration.
+    Caffe2 core uses the min of caffe2_log_level and minloglevel
+    to determine loglevel.
+    For more info see UpdateLoggingLevelsFromFlags() in caffe2/caffe2/core/logging.cc
+    '''
+    core.GlobalInit(["caffe2", "--caffe2_log_level=2", "--minloglevel=2"])
+
     logger.info("Running gym with params")
     logger.info(params)
     rl_parameters = RLParameters(**params["rl"])
