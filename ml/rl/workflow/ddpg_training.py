cab76b2b9bdc88cd8180d7fd2e1d11e40d31d913
Edoardo Conti <edoardoc@fb.com>
2018-08-17 10:08:29 -0700

    Add script to dump hive table to file and add scaffolding for open source workflows
    Summary: title
    
    Reviewed By: MisterTea
    
    Differential Revision: D9367373
    
    fbshipit-source-id: 78f5c08a34c579f37cc6168347aba22a50000522

---
+++ ml/rl/workflow/ddpg_training.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# End to end DDPG trainer for batch data
+
+
+def train_network():
+    pass
---
+++ ml/rl/workflow/dqn_training.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# End to end DQN trainer for batch data
+
+
+def train_network():
+    pass
