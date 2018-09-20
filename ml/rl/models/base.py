f42e63f4dd4492dc69d9f002b385490d3f39ee3d
Kittipat Virochsiri <kittipat@fb.com>
2018-09-19 17:19:09 -0700

    Implement model export w/ feature extractor
    Summary: This will allow exporting PyTorch model with Caffe2 feature extractor as long as the input/output are compatible. PyTorch constants are also saved automatically here.
    
    Reviewed By: MisterTea
    
    Differential Revision: D9889044
    
    fbshipit-source-id: 670fc338cbf3fe8fff4a6e0266c4ac76c46a049c

--- ml/rl/models/base.py
+++ ml/rl/models/base.py
@@ -5,8 +5,12 @@ from collections import OrderedDict
 from io import BytesIO
 from typing import List, Tuple
 
+import caffe2.python.onnx.backend
+import onnx
 import torch
 import torch.nn as nn
+from caffe2.python import core
+from caffe2.python.predictor.predictor_exporter import PredictorExportMeta
 
 
 class ModelBase(nn.Module, metaclass=abc.ABCMeta):
@@ -34,6 +38,43 @@ class ModelBase(nn.Module, metaclass=abc.ABCMeta):
         """
         raise NotImplemented
 
+    def input_blob_names(self) -> List[str]:
+        """
+        Returns the names of the input blobs. If `None`, the names will be
+        derived from `self.input_prototype`.
+        """
+        return self.derived_input_blob_names()
+
+    def derived_input_blob_names(self) -> List[str]:
+        return self.derive_blob_names(self.input_prototype())
+
+    def derive_blob_names(self, named_tuple):
+        def name_helper(d):
+            if hasattr(d, "_asdict"):
+                d = d._asdict()
+            if isinstance(d, OrderedDict):
+                return [
+                    ":".join(filter(None, [k, n]))
+                    for k, v in d.items()
+                    for n in filter(lambda x: x is not None, name_helper(v))
+                ]
+            elif isinstance(d, torch.Tensor):
+                return [""]
+            elif d is None:
+                return [None]
+            else:
+                raise ValueError()
+
+        return list(filter(None, name_helper(named_tuple)))
+
+    def output_blob_names(self) -> List[str]:
+        """
+        ONNX graph doesn't have good naming.
+
+        Returns the names of the output blobs.
+        """
+        return self.derive_blob_names(self(self.input_prototype()))
+
     def export_to_buffer(self) -> BytesIO:
         """
         Export the instance to BytesIO buffer
@@ -47,6 +88,132 @@ class ModelBase(nn.Module, metaclass=abc.ABCMeta):
             input_names=export_model.onnx_input_names(),
         )
         return write_buffer
+
+    def get_caffe2_model(self):
+        model_protobuf = onnx.load(BytesIO(self.export_to_buffer().getvalue()))
+        input_blobs = [i.name for i in model_protobuf.graph.input]
+        output_blobs = [o.name for o in model_protobuf.graph.output]
+        return (
+            caffe2.python.onnx.backend.prepare(model_protobuf),
+            input_blobs,
+            output_blobs,
+        )
+
+    def get_predictor_export_meta_and_workspace(self, feature_extractor=None):
+        """
+
+        ONNX would load blobs into a private workspace. We returns the workspace
+        here instead of copying the blobs to the global workspace in order to
+        save memory in the export state. Returning private workspace, we only
+        need memory for PyTorch model, ONNX buffer, and Caffe2 model. Including
+        optimizer parameters, this means we can train and save a model
+        a quarter of the size of machine memory.
+
+        We should revisit this once PyTorch 1.0 is ready.
+
+        Args:
+            feature_extractor: An instance of FeatureExtractorBase
+        """
+        # 1. Get Caffe2 model
+        c2_model, input_blobs, output_blobs = self.get_caffe2_model()
+        ws = c2_model.workspace
+
+        # Initializing constants in the model
+        init_net = core.Net(c2_model.init_net)
+        ws.CreateNet(init_net)
+        ws.RunNet(init_net)
+
+        # Per ONNX code comment, input blobs are not initilized
+        model_inputs = c2_model.uninitialized
+        assert len(model_inputs) > 0, "Model is expected to have some input"
+        parameters = [b for b in ws.Blobs() if b not in model_inputs]
+        # Input blobs in order
+        model_input_blobs = [b for b in input_blobs if b in model_inputs]
+
+        predict_net = core.Net("predict_net")
+
+        output_blob_names = self.output_blob_names()
+        assert len(output_blobs) == len(output_blob_names), (
+            "output_blobs and output_blob_names must have the same lengths. "
+            "Check that your model don't reuse output tensors. "
+            "output_blobs: {}; output_blob_names: {}".format(
+                output_blobs, output_blob_names
+            )
+        )
+        blob_remap = {
+            onnx_name: explicit_name
+            for onnx_name, explicit_name in zip(output_blobs, output_blob_names)
+        }
+        shapes = {}
+
+        # 2. Create feature extractor net
+        if feature_extractor:
+            feature_extractor_nets = feature_extractor.create_net()
+            # Initializing feature extractor parameters
+            ws.CreateNet(feature_extractor_nets.init_net)
+            ws.RunNet(feature_extractor_nets.init_net)
+            feature_extractor_params = set(
+                feature_extractor_nets.init_net.Proto().external_output
+            )
+            assert (
+                len(set(parameters) & feature_extractor_params) == 0
+            ), "Blob names collide! Please open a bug report"
+            parameters += feature_extractor_params
+            extracted_blobs = [
+                str(b) for b in feature_extractor_nets.net.output_record().field_blobs()
+            ]
+            assert len(model_input_blobs) == len(extracted_blobs), (
+                "The lengths of model_input_blobs and extracted_blobs must match. "
+                "model_input_blobs: {}; extracted_blobs: {}".format(
+                    model_input_blobs, extracted_blobs
+                )
+            )
+            blob_remap.update(
+                {
+                    onnx_name: extracted_name
+                    for onnx_name, extracted_name in zip(
+                        model_input_blobs, extracted_blobs
+                    )
+                }
+            )
+
+            predict_net.AppendNet(feature_extractor_nets.net)
+            del predict_net.Proto().external_output[:]
+
+            input_blobs = [
+                b
+                for b in predict_net.Proto().external_input
+                if b not in feature_extractor_params
+            ]
+            shapes.update({b: [] for b in input_blobs})
+        else:
+            input_blobs = model_input_blobs
+
+        # 3. Rename the input blobs of model to match the output of feature
+        #    extractor net
+
+        model_net = core.Net(c2_model.predict_net).Clone(
+            "remapped_model_net", blob_remap=blob_remap
+        )
+
+        # 5. Join feature extractor net & model net
+        predict_net.AppendNet(model_net)
+
+        # These shapes are not really used but required, so just pass fake ones
+        shapes.update({b: [] for b in predict_net.Proto().external_output})
+
+        return (
+            PredictorExportMeta(
+                predict_net,
+                parameters,
+                input_blobs,
+                predict_net.Proto().external_output,
+                shapes=shapes,
+                net_type="async_scheduling",
+                num_workers=8,
+            ),
+            ws,
+        )
 
 
 class ONNXExportModel(nn.Module):
@@ -70,23 +237,14 @@ class ONNXExportModel(nn.Module):
         The names are the path to the tensor joint by ":".
         """
 
-        def name_helper(d):
-            if hasattr(d, "_asdict"):
-                d = d._asdict()
-            if isinstance(d, OrderedDict):
-                return [
-                    ":".join(filter(None, [k, n]))
-                    for k, v in d.items()
-                    for n in filter(lambda x: x is not None, name_helper(v))
-                ]
-            elif isinstance(d, torch.Tensor):
-                return [""]
-            elif d is None:
-                return [None]
-            else:
-                raise ValueError()
+        explicit_names = self.m.input_blob_names()
+        derived_names = self.m.derived_input_blob_names()
 
-        return list(filter(None, name_helper(self.input_prototype)))
+        if explicit_names is not None:
+            assert len(explicit_names) == len(derived_names)
+            return explicit_names
+
+        return derived_names
 
     def structurize_input(self, args) -> OrderedDict:
         """
--- ml/rl/test/models/test_base.py
+++ ml/rl/test/models/test_base.py
@@ -1,34 +1,166 @@
 #!/usr/bin/env python3
 
+import logging
+import os
+import tempfile
 import unittest
 from io import BytesIO
+from typing import Any, NamedTuple
 
+import numpy as np
+import numpy.testing as npt
 import onnx
 import torch
+import torch.nn as nn
+from caffe2.python import workspace
+from caffe2.python.predictor.predictor_exporter import (
+    prepare_prediction_net,
+    save_to_db,
+)
 from ml.rl.models.base import ModelBase
+from ml.rl.preprocessing.feature_extractor import PredictorFeatureExtractor
+from ml.rl.preprocessing.identify_types import CONTINUOUS
+from ml.rl.preprocessing.normalization import NormalizationParameters
 from ml.rl.types import FeatureVector, StateAction
+
+
+logger = logging.getLogger(__name__)
+
+
+class ModelOutput(NamedTuple):
+    # These should be torch.Tensor but the type checking failed when I used it
+    sum: Any
+    mul: Any
+    plus_one: Any
+    linear: Any
 
 
 class Model(ModelBase):
     def __init__(self):
         super(Model, self).__init__()
+        self.linear = nn.Linear(4, 1)
 
     def input_prototype(self):
         return StateAction(
-            state=FeatureVector(float_features=torch.zeros([1, 4])),
-            action=FeatureVector(float_features=torch.zeros([1, 4])),
+            state=FeatureVector(float_features=torch.randn([1, 4])),
+            action=FeatureVector(float_features=torch.randn([1, 4])),
         )
 
     def forward(self, sa):
-        return sa.state.float_features + sa.action.float_features
+        return ModelOutput(
+            sa.state.float_features + sa.action.float_features,
+            sa.state.float_features * sa.action.float_features,
+            sa.state.float_features + 1,
+            self.linear(sa.state.float_features),
+        )
 
 
 class TestBase(unittest.TestCase):
-    def test_export(self):
+    def test_export_to_buffer(self):
         model = Model()
         buffer = model.export_to_buffer()
         protobuf_model = onnx.load(BytesIO(buffer.getvalue()))
-        self.assertEqual(2, len(protobuf_model.graph.input))
-        self.assertEqual(1, len(protobuf_model.graph.output))
+        self.assertEqual(4, len(protobuf_model.graph.input))  # 2 inputs + 2 params
+        self.assertEqual(4, len(protobuf_model.graph.output))
         self.assertEqual("state:float_features", protobuf_model.graph.input[0].name)
         self.assertEqual("action:float_features", protobuf_model.graph.input[1].name)
+
+    def test_get_predictor_export_meta_and_workspace(self):
+        model = Model()
+        pem, ws = model.get_predictor_export_meta_and_workspace()
+        self.assertEqual(3, len(pem.parameters))  # 2 params + 1 const
+        for p in pem.parameters:
+            self.assertTrue(ws.HasBlob(p))
+        self.assertEqual(2, len(pem.inputs))
+        self.assertEqual(4, len(pem.outputs))
+
+        input_prototype = model.input_prototype()
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            db_path = os.path.join(tmpdirname, "model")
+            logger.info("DB path: ", db_path)
+            db_type = "minidb"
+            with ws._ctx:
+                save_to_db(db_type, db_path, pem)
+
+            # Load the model from DB file and run it
+            net = prepare_prediction_net(db_path, db_type)
+
+            state_features = input_prototype.state.float_features.numpy()
+            action_features = input_prototype.action.float_features.numpy()
+            workspace.FeedBlob("state:float_features", state_features)
+            workspace.FeedBlob("action:float_features", action_features)
+            workspace.RunNet(net)
+            net_sum = workspace.FetchBlob("sum")
+            net_mul = workspace.FetchBlob("mul")
+            net_plus_one = workspace.FetchBlob("plus_one")
+            net_linear = workspace.FetchBlob("linear")
+
+            model_sum, model_mul, model_plus_one, model_linear = model(input_prototype)
+
+            npt.assert_array_equal(model_sum.numpy(), net_sum)
+            npt.assert_array_equal(model_mul.numpy(), net_mul)
+            npt.assert_array_equal(model_plus_one.numpy(), net_plus_one)
+            npt.assert_array_equal(model_linear.detach().numpy(), net_linear)
+
+    def test_get_predictor_export_meta_and_workspace_with_feature_extractor(self):
+        model = Model()
+
+        state_normalization_parameters = {
+            i: NormalizationParameters(feature_type=CONTINUOUS) for i in range(1, 5)
+        }
+        action_normalization_parameters = {
+            i: NormalizationParameters(feature_type=CONTINUOUS) for i in range(5, 9)
+        }
+
+        extractor = PredictorFeatureExtractor(
+            state_normalization_parameters=state_normalization_parameters,
+            action_normalization_parameters=action_normalization_parameters,
+        )
+
+        pem, ws = model.get_predictor_export_meta_and_workspace(
+            feature_extractor=extractor
+        )
+        # model has 2 params + 1 const. extractor has 1 const.
+        self.assertEqual(4, len(pem.parameters))
+        for p in pem.parameters:
+            self.assertTrue(ws.HasBlob(p))
+        self.assertEqual(3, len(pem.inputs))
+        self.assertEqual(4, len(pem.outputs))
+
+        input_prototype = model.input_prototype()
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            db_path = os.path.join(tmpdirname, "model")
+            logger.info("DB path: ", db_path)
+            db_type = "minidb"
+            with ws._ctx:
+                save_to_db(db_type, db_path, pem)
+
+            # Load the model from DB file and run it
+            net = prepare_prediction_net(db_path, db_type)
+
+            state_features = input_prototype.state.float_features
+            action_features = input_prototype.action.float_features
+            float_features_values = (
+                torch.cat((state_features, action_features), dim=1).reshape(-1).numpy()
+            )
+            float_features_keys = np.arange(1, 9)
+            float_features_lengths = np.array([8], dtype=np.int32)
+
+            workspace.FeedBlob("input/float_features.keys", float_features_keys)
+            workspace.FeedBlob("input/float_features.values", float_features_values)
+            workspace.FeedBlob("input/float_features.lengths", float_features_lengths)
+
+            workspace.RunNet(net)
+            net_sum = workspace.FetchBlob("sum")
+            net_mul = workspace.FetchBlob("mul")
+            net_plus_one = workspace.FetchBlob("plus_one")
+            net_linear = workspace.FetchBlob("linear")
+
+            model_sum, model_mul, model_plus_one, model_linear = model(input_prototype)
+
+            npt.assert_array_equal(model_sum.numpy(), net_sum)
+            npt.assert_array_equal(model_mul.numpy(), net_mul)
+            npt.assert_array_equal(model_plus_one.numpy(), net_plus_one)
+            npt.assert_array_equal(model_linear.detach().numpy(), net_linear)
