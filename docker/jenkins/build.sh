11135c9f99e2a6fd942867bb33bec92687b02207
kittipatv <kittipatv@users.noreply.github.com>
2018-09-24 19:44:23 -0700

    Ci setup (#23)
    Adding files necessary for CI

---
+++ docker/jenkins/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Builds Docker image.
+
+set -ex
+
+image="$1"
+shift
+
+if [ -z "${image}" ]; then
+  echo "Usage: $0 IMAGE"
+  exit 1
+fi
+
+USE_GPU=""
+if [[ "$image" == *-cuda* ]]; then
+  USE_GPU="1"
+fi
+
+# Set Jenkins UID and GID if running Jenkins
+if [ -n "${JENKINS:-}" ]; then
+  JENKINS_UID=$(id -u jenkins)
+  JENKINS_GID=$(id -g jenkins)
+fi
+
+docker build \
+  --no-cache \
+  --build-arg "JENKINS=${JENKINS:-}" \
+  --build-arg "JENKINS_UID=${JENKINS_UID:-}" \
+  --build-arg "JENKINS_GID=${JENKINS_GID:-}" \
+  --build-arg "USE_GPU=${USE_GPU}" \
+  "$@" \
+  .
