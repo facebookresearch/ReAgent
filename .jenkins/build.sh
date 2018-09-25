3122ce9a85632a29a461b5c2c4e95eef373999c4
Kittipat Virochsiri <kittipat@fb.com>
2018-09-25 00:00:04 -0700

    Fixing build

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -4,6 +4,7 @@
 pip uninstall -y horizon
 
 thrift --gen py:json --out . ml/rl/thrift/core.thrift
+thrift --gen py:json --out . ml/rl/thrift/eval.thrift
 
 sudo python3 setup.py build develop
 python3 setup.py test
