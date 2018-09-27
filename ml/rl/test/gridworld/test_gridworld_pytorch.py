1baf84060a1cc12857eafc66b993c8ab9ce73df0
Edoardo Conti <edoardoc@fb.com>
2018-09-26 21:00:50 -0700

    Add test to test q-values before and after exporting
    Reviewed By: MisterTea
    
    Differential Revision: D10080592
    
    fbshipit-source-id: 6d8db989e8cf9e1e6ae7bf84861d2f4f6d7419ed

--- ml/rl/test/gridworld/test_gridworld_pytorch.py
+++ ml/rl/test/gridworld/test_gridworld_pytorch.py
@@ -8,7 +8,7 @@ import unittest
 import numpy as np
 import torch
 from ml.rl.test.gridworld.gridworld import Gridworld
-from ml.rl.test.gridworld.gridworld_base import DISCOUNT
+from ml.rl.test.gridworld.gridworld_base import DISCOUNT, Samples
 from ml.rl.test.gridworld.gridworld_enum import GridworldEnum
 from ml.rl.test.gridworld.gridworld_evaluator import GridworldEvaluator
 from ml.rl.thrift.core.ttypes import (
@@ -165,3 +165,40 @@ class TestGridworld(unittest.TestCase):
             evaluator.value_doubly_robust[-1],
         )
         self.assertLess(evaluator.mc_loss[-1], 0.1)
+
+    def test_predictor_export(self):
+        """Verify that q-values before model export equal q-values after
+        model export. Meant to catch issues with export logic."""
+        environment = Gridworld()
+        trainer = trainer = self.get_sarsa_trainer(environment, False)
+
+        samples = Samples(
+            mdp_ids=["0"],
+            sequence_numbers=[0],
+            states=[{0: 1.0, 1: 1.0, 2: 1.0, 3: 1.0, 4: 1.0, 5: 1.0, 15: 1.0, 24: 1.0}],
+            actions=["D"],
+            action_probabilities=[.5],
+            rewards=[0],
+            possible_actions=[["R", "D"]],
+            next_states=[{5: 1.0}],
+            next_actions=["U"],
+            terminals=[False],
+            possible_next_actions=[["R", "U", "D"]],
+            episode_values=[0.00161],
+        )
+        tdps = environment.preprocess_samples(samples, 1)
+
+        pre_export_q_values = trainer.q_network(tdps[0].states).detach().numpy()
+
+        predictor = trainer.predictor()
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            tmp_path = os.path.join(tmpdirname, "model")
+            predictor.save(tmp_path, "minidb")
+            new_predictor = DQNPredictor.load(tmp_path, "minidb", False)
+
+        post_export_q_values = new_predictor.predict([samples.states[0]])
+
+        for i, action in enumerate(environment.ACTIONS):
+            self.assertAlmostEquals(
+                pre_export_q_values[0][i], post_export_q_values[0][action], places=4
+            )
