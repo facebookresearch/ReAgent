a10476f01811bf63fbb5043d5a4fbefa4d9b5d56
Kittipat Virochsiri <kittipat@fb.com>
2018-09-28 16:44:38 -0700

    Install guide
    Summary: Pull Request resolved: https://github.com/facebookresearch/Horizon/pull/33
    
    Reviewed By: econti
    
    Differential Revision: D10100800
    
    Pulled By: kittipatv
    
    fbshipit-source-id: 258ba9fb0026858ca1cbec84a8602b8a71f416d7

---
+++ install_compilers.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+wget https://archive.apache.org/dist/thrift/0.11.0/thrift-0.11.0.tar.gz -O thrift-0.11.0.tar.gz
+tar -xzf thrift-0.11.0.tar.gz
+cd thrift-0.11.0
+./bootstrap.sh
+./configure
+make
+make install
+rm -rf thrift-0.11.0
+rm thrift-0.11.0.tar.gz
+
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
+apt-get install -y oracle-java8-installer maven
