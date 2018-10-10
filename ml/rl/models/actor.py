df07d17b6d90502c49046abcbdba160fd962fdc4
Kittipat Virochsiri <kittipat@fb.com>
2018-10-10 13:23:55 -0700

    Implement FullyConnectedActor model
    Summary: In preparation for flexible model architecture
    
    Reviewed By: MisterTea
    
    Differential Revision: D10288059
    
    fbshipit-source-id: 9fcbae62997184579de5d29e6b772c80e9c31ea1

---
+++ ml/rl/models/actor.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import torch
+from ml.rl import types as rlt
+from ml.rl.models.base import ModelBase
+from ml.rl.models.fully_connected_network import FullyConnectedNetwork
+
+
+class FullyConnectedActor(ModelBase):
+    def __init__(
+        self,
+        state_dim,
+        action_dim,
+        sizes,
+        activations,
+        use_batch_norm=False,
+        action_activation="tanh",
+    ):
+        super(FullyConnectedActor, self).__init__()
+        assert state_dim > 0, "state_dim must be > 0, got {}".format(state_dim)
+        assert action_dim > 0, "action_dim must be > 0, got {}".format(action_dim)
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        assert len(sizes) == len(
+            activations
+        ), "The numbers of sizes and activations must match; got {} vs {}".format(
+            len(sizes), len(activations)
+        )
+        self.action_activation = action_activation
+        self.fc = FullyConnectedNetwork(
+            [state_dim] + sizes + [action_dim],
+            activations + [self.action_activation],
+            use_batch_norm=use_batch_norm,
+        )
+
+    def input_prototype(self):
+        return rlt.State(float_features=torch.randn(1, self.state_dim))
+
+    def forward(self, input):
+        action = self.fc(input.float_features)
+        return rlt.ParametricAction(float_features=action)
