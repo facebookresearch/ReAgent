db7465d92c5b054ed8f0831c37e8306485735dd0
Jason Gauci <jjg@fb.com>
2018-10-16 10:38:00 -0700

    Minor tweaks to scripts
    Summary: Pull Request resolved: https://github.com/facebookresearch/Horizon/pull/38
    
    Reviewed By: kittipatv
    
    Differential Revision: D10403632
    
    Pulled By: MisterTea
    
    fbshipit-source-id: 0be0bfe01b18c261025f6ef6f4c529fe2c2e006d

--- scripts/gen_training_data.sh
+++ scripts/gen_training_data.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-python ml/rl/test/gym/run_gym.py -p ml/rl/test/gym/discrete_dqn_cartpole_v0_100_eps.json -f ~/training_data.json
+mkdir cartpole_discrete
+
+python ml/rl/test/gym/run_gym.py -p ml/rl/test/gym/discrete_dqn_cartpole_v0_100_eps.json -f cartpole_discrete/training_data.json
