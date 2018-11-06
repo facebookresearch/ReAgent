8c5b7565a47d0f4267c84dbffb7f97fe278ba3dd
Jason Gauci <jjg@fb.com>
2018-11-05 19:29:17 -0800

    Cleanup some thrift and install cython first

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-# Generate thrift specs
-thrift --gen py --out . ml/rl/thrift/core.thrift
-
 # Install the current directory into python path
 pip install -e .
 
 # Run workflow tests
-python -m ml.rl.test.workflow.test_oss_workflows
+python -m unittest -b ml.rl.test.workflow.test_oss_workflows
