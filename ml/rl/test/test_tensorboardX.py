959d313ff0b41af9a41e390a78b3fe476d218d33
Jason Gauci <jjg@fb.com>
2018-11-16 13:11:12 -0800

    fix tensorboard test
    Summary: Pull Request resolved: https://github.com/facebookresearch/Horizon/pull/71
    
    Differential Revision: D13104552
    
    Pulled By: MisterTea
    
    fbshipit-source-id: 09e4b992aa3075678385ee07ac833dab2dbd17de

--- ml/rl/test/test_tensorboardX.py
+++ ml/rl/test/test_tensorboardX.py
@@ -11,6 +11,12 @@ from tensorboardX import SummaryWriter
 
 
 class TestSummaryWriterContext(unittest.TestCase):
+    def setUp(self):
+        SummaryWriterContext._reset_globals()
+
+    def tearDown(self):
+        SummaryWriterContext._reset_globals()
+
     def test_noop(self):
         self.assertIsNone(SummaryWriterContext.add_scalar("test", torch.ones(1)))
 
