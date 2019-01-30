Fix inf bug in masked softmax
Summary:
if a masked action has a higher q-value than the max q-value of a non-masked action, we can hit a case where post softmax we get infinity / nan which leads to nan in CPE. Example:

```
(Pdb) x[0]
tensor([ 1.5296,  1.5991, -9.7862,  0.6222,  0.5375, -0.8296, -0.6833, -0.7080,
         1.9742,  0.3508,  0.6671,  2.5219, -2.9556, -4.7005,  0.5733, -0.8476,
         0.2150, -4.2360, -0.7432,  4.3746, -1.5712, -3.6860,  3.4471])
(Pdb) x = x / temperature
(Pdb) x[0]
tensor([ 152.9619,  159.9104, -978.6201,   62.2182,   53.7526,  -82.9575,
         -68.3296,  -70.8034,  197.4220,   35.0787,   66.7079,  252.1872,
        -295.5598, -470.0481,   57.3292,  -84.7643,   21.5047, -423.6021,
         -74.3194,  437.4622, -157.1228, -368.6041,  344.7051])
(Pdb) mask_min_x[0]
tensor([-1.0000e+20, -1.0000e+20, -1.0000e+20, -1.0000e+20, -1.0000e+20,
                 -1.0000e+20, -1.0000e+20, -1.0000e+20, -1.0000e+20, -1.0000e+20,
                 -1.0000e+20, -1.0000e+20, -1.0000e+20, -1.0000e+20,  5.7329e+01,
                 -1.0000e+20, -1.0000e+20, -1.0000e+20, -1.0000e+20, -1.0000e+20,
                 -1.0000e+20, -1.0000e+20,  3.4471e+02])
(Pdb) torch.max(mask_min_x, dim=1, keepdim=True)[0][0]
tensor([344.7051])
(Pdb) x -= torch.max(mask_min_x, dim=1, keepdim=True)[0]
(Pdb) x[0]
tensor([ -191.7432,  -184.7948, -1323.3252,  -282.4869,  -290.9526,  -427.6627,
         -413.0348,  -415.5086,  -147.2832,  -309.6264,  -277.9972,   -92.5180,
         -640.2649,  -814.7532,  -287.3759,  -429.4695,  -323.2004,  -768.3073,
         -419.0246,    92.7571,  -501.8279,  -713.3093,     0.0000])
(Pdb) e_x = torch.exp(x)
(Pdb) e_x[0]
tensor([0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00,
        0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00, 6.6063e-41,
        0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00,
        0.0000e+00,        inf, 0.0000e+00, 0.0000e+00, 1.0000e+00])
```

Reviewed By

ml/rl/caffe_utils.py
@@ -307,7 +307,11 @@ def masked_softmax(x, mask, temperature):
     """Compute softmax values for each sets of scores in x."""
     x = x / temperature
     mask_min_x = x - ((1.0 - mask) * 1e20)
-    x -= torch.max(mask_min_x, dim=1, keepdim=True)[0]
-    e_x = torch.exp(x)
+    mask_min_x -= torch.max(mask_min_x, dim=1, keepdim=True)[0]
+    e_x = torch.exp(mask_min_x)
     e_x *= mask
-    return e_x / e_x.sum(dim=1, keepdim=True)
+    out = e_x / e_x.sum(dim=1, keepdim=True)
+
+    # Set NaN values to 0 (NaN happens when a full mask row is passed in)
+    out[out != out] = 0
+    return out