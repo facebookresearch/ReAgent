babe85cfe62d9f027d4aee3baa126314078c9b0b
Kittipat Virochsiri <kittipat@fb.com>
2019-03-16 19:21:44 -0700

    Example of using sequence in a model
    Summary: This is a small example showing how we could add RNN (GRU) in our model. The feature extraction API will need more work. Model definition will be done programmatically for each use cases (to avoid the configuration hell).
    
    Reviewed By: czxttkl
    
    Differential Revision: D14362364
    
    fbshipit-source-id: 0ab675d350e0c5ad0f1aefd82c089498113ac065

--- ml/rl/types.py
+++ ml/rl/types.py
@@ -11,7 +11,9 @@ than dictionary; so, this should be good overall.
 
 """
 
-from typing import Any, NamedTuple, Optional, Union
+import dataclasses
+from dataclasses import dataclass
+from typing import Any, Dict, List, NamedTuple, Optional, Type, TypeVar, Union
 
 import numpy as np
 import torch
@@ -25,18 +27,124 @@ type of value
 ValueType = Union[torch.Tensor, core.BlobReference, np.ndarray]
 
 
-class IdListFeature(NamedTuple):
-    lengths: ValueType
-    values: ValueType
+@dataclass
+class IdFeatureConfig:
+    """
+    This describes how to map raw features to model features
+    """
+
+    feature_id: int  # integer feature ID
+    id_mapping_name: str  # key to ModelPreprocessingConfig.id_mapping_config
+
+
+@dataclass
+class IdFeatureBase:
+    """
+    User should subclass this class and define each ID feature as a field w/ ValueType
+    as the type of the field.
+    """
+
+    @classmethod
+    # TODO: This should be marked as abstractmethod but mypi doesn't like it.
+    # See https://github.com/python/mypy/issues/5374
+    # @abc.abstractmethod
+    def get_feature_config(cls) -> Dict[str, IdFeatureConfig]:
+        """
+        Returns mapping from feature name, which must be a field in this dataclass, to
+        feature config.
+        """
+        raise NotImplementedError
+
+
+T = TypeVar("T", bound="SequenceFeatureBase")
+
+
+@dataclass
+class SequenceFeatureBase:
+    id_features: Optional[IdFeatureBase]
+    float_features: Optional[ValueType]
+
+    @classmethod
+    # TODO: This should be marked as abstractmethod but mypi doesn't like it.
+    # See https://github.com/python/mypy/issues/5374
+    # @abc.abstractmethod
+    def get_max_length(cls) -> int:
+        """
+        Subclass should return the max-length of this sequence. If the raw data is
+        longer, feature extractor will truncate the front. If the raw data is shorter,
+        feature extractor will fill the front with zero.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def get_float_feature_ids(cls) -> List[int]:
+        """
+        Override this if the sequence has float features associated to it.
+        Float features should be stored as ID-score-list, where the ID part corresponds
+        to primary entity ID of the sequence. E.g., if this is a sequence of previously
+        watched videos, then the key should be video ID.
+        """
+        return []
+
+    @classmethod
+    def prototype(cls: Type[T]) -> T:
+        float_feature_ids = cls.get_float_feature_ids()
+        float_features = (
+            torch.rand(1, cls.get_max_length(), len(float_feature_ids))
+            if float_feature_ids
+            else None
+        )
+        fields = dataclasses.fields(cls)
+        id_features = None
+        for field in fields:
+            if field.name != "id_features" or not issubclass(field.type, IdFeatureBase):
+                continue
+            id_feature_fields = dataclasses.fields(field.type)
+            id_features = field.type(  # noqa
+                **{
+                    f.name: torch.randint(1, (1, cls.get_max_length()))
+                    for f in id_feature_fields
+                }
+            )
+            break
+
+        return cls(id_features=id_features, float_features=float_features)
+
+
+U = TypeVar("U", bound="SequenceFeatures")
+
+
+@dataclass
+class SequenceFeatures:
+    """
+    A stub-class for sequence features in the model. All fileds should be subclass of
+    SequenceFeatureBase above.
+    """
+
+    @classmethod
+    def prototype(cls: Type[U]) -> U:
+        fields = dataclasses.fields(cls)
+        return cls(**{f.name: f.type.prototype() for f in fields})  # noqa
+
+
+@dataclass
+class IdMapping:
+    ids: List[int]
+
+
+@dataclass
+class ModelFeatureConfig:
+    id_mapping_config: Dict[str, IdMapping]
+    sequence_features_type: Type[SequenceFeatures]
 
 
 class FeatureVector(NamedTuple):
     float_features: Optional[ValueType] = None
-    # id_list_features should ideally be Mapping[str, IdListFeature]; however,
+    # sequence_features should ideally be Mapping[str, IdListFeature]; however,
     # that doesn't work well with ONNX.
     # User is expected to dynamically define the type of id_list_features based
     # on the actual features used in the model.
-    id_list_features: Optional[NamedTuple] = None
+    sequence_features: Optional[SequenceFeatureBase] = None
 
 
 DiscreteAction = ValueType
