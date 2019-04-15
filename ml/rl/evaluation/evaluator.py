Fix bugs in CPE calculation in DQN
Summary:
This diff attempts to fix bugs in CPE calculation for discrete-action DQN.

We collect gridworld data based on a 0.25-epsilon policy (i.e., 75% of time we take optimal actions otherwise random). In gridworld, the true cpe result can be well computed by existing code:
        gamma = 0.9
        true_logged_value = env.true_q_epsilon_values(gamma, 0.25)[0]
        # The learned policy should be close to optimal, which is equivalent
        # to a policy with epsilon=0.0
        true_model_value = env.true_q_epsilon_values(gamma, 0.0)[0]
        true_cpe = true_model_value / true_logged_value
        # result: 1.2412182093520276

Before this diff, the cpe results on Gridworld are (from f107132830):

{F155321950}

reward_ips, reward_dm and reward_dr are are close to 1.24 (computed true cpe) as indicated in the screenshot above.

However, sequential CPEs are not close to 1.24, indicating some possible bugs.

We've identified and fixed two incorrect pieces of code:
1. in `evaluation_data_page.py`, where model values should be computed by calling `q_network_cpe`
2. in `dqn_trainer.py`, where q_network_cpe should be fitted using
{F155322380}

We also scale the terminal reward in Gridworld to 10 (original was 1). We observed that a larger reward scale makes the ratio  `td_loss/(reward_scale ** 2)` smaller and CPE more stable.

Reviewed By: econti

Differential Revision: D14751938

fbshipit-source-id: 976adaf39bba450c1a4b7b9e2c75dbb6f08adf7d

ml/rl/evaluation/evaluator.py
@@ -88,18 +88,18 @@ class Evaluator(object):
                     metric, metric_reward_edp
                 )
 
-        if self.action_names is not None:
-            value_means = edp.model_values.mean(dim=0)
+        if self.action_names is not None and edp.optimal_q_values is not None:
+            value_means = edp.optimal_q_values.mean(dim=0)
             cpe_details.q_value_means = {
                 action: float(value_means[i])
                 for i, action in enumerate(self.action_names)
             }
-            value_stds = edp.model_values.std(dim=0)
+            value_stds = edp.optimal_q_values.std(dim=0)
             cpe_details.q_value_stds = {
                 action: float(value_stds[i])
                 for i, action in enumerate(self.action_names)
             }
-            max_q_idxs = edp.model_values.argmax(dim=1)
+            max_q_idxs = edp.optimal_q_values.argmax(dim=1)
             cpe_details.action_distribution = {
                 action: float((max_q_idxs == i).sum()) / max_q_idxs.shape[0]
                 for i, action in enumerate(self.action_names)