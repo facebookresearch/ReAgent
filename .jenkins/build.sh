d9f654a1b996b22479b4fe7a1d6d9a76819166d2
Kittipat Virochsiri <kittipat@fb.com>
2019-10-15 16:48:09 -0700

    CircleCI config
    Summary: Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/167
    
    Differential Revision: D17943315
    
    Pulled By: kittipatv
    
    fbshipit-source-id: 1dff954dc821bc33e62fd6dafac8460d08c2b930

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -8,8 +8,6 @@ export PATH=${HOME}/miniconda/bin:$PATH
 
 pip uninstall -y horizon
 
-thrift --gen py --out . ml/rl/thrift/core.thrift
-
 # Installing from current directory, any update will be reflected system-wide
 pip install -e .
 pytest
