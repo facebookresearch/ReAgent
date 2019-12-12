9cf8f6cdf6a2008843cb37da6e34b8d10353b0bf
Kittipat Virochsiri <kittipat@fb.com>
2019-12-12 09:05:37 -0800

    Use DataFrame to speed up PythonSparseToDenseProcessor
    Summary: DataFrame actually does a pretty good job at sparse2dense
    
    Reviewed By: czxttkl
    
    Differential Revision: D18903996
    
    fbshipit-source-id: a1ad7c539671375c44a0a57d2055717a135f2a27

--- ml/rl/preprocessing/sparse_to_dense.py
+++ ml/rl/preprocessing/sparse_to_dense.py
@@ -2,16 +2,12 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
 
 
-import logging
 from typing import Dict, List, Tuple
 
 # @manual=third-party//pandas:pandas-py
 import pandas as pd
 import torch
 from ml.rl.preprocessing import normalization
-
-
-logger = logging.getLogger(__name__)
 
 
 class SparseToDenseProcessor:
@@ -26,13 +22,17 @@ class SparseToDenseProcessor:
 
 
 class PandasSparseToDenseProcessor(SparseToDenseProcessor):
+    """
+    We just have this in case the input data is keyed by string
+    """
+
     def __init__(
         self, sorted_features: List[int], set_missing_value_to_zero: bool = False
     ):
         super().__init__(sorted_features, set_missing_value_to_zero)
-        self.feature_to_index: Dict[int, int] = {}
-        for i, f in enumerate(sorted_features):
-            self.feature_to_index[f] = i
+        self._sparse_to_dense = PythonSparseToDenseProcessor(
+            sorted_features, set_missing_value_to_zero
+        )
 
     def process(self, sparse_data) -> Tuple[torch.Tensor, torch.Tensor]:
         # Convert all keys to integers
@@ -40,12 +40,27 @@ class PandasSparseToDenseProcessor(SparseToDenseProcessor):
         for sd in sparse_data:
             sd_int = {}
             for k, v in sd.items():
-                sd_int[self.feature_to_index[int(k)]] = v
+                sd_int[self._sparse_to_dense.feature_to_index[int(k)]] = v
             sparse_data_int.append(sd_int)
+        return self._sparse_to_dense(sparse_data_int)
+
+
+class PythonSparseToDenseProcessor(SparseToDenseProcessor):
+    def __init__(
+        self, sorted_features: List[int], set_missing_value_to_zero: bool = False
+    ):
+        super().__init__(sorted_features, set_missing_value_to_zero)
+        self.feature_to_index: Dict[int, int] = {
+            f: i for i, f in enumerate(sorted_features)
+        }
+
+    def process(
+        self, sparse_data: List[Dict[int, float]]
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
         missing_value = normalization.MISSING_VALUE
         if self.set_missing_value_to_zero:
             missing_value = 0.0
-        state_features_df = pd.DataFrame(sparse_data_int).fillna(missing_value)
+        state_features_df = pd.DataFrame(sparse_data).fillna(missing_value)
         # Add columns identified by normalization, but not present in batch
         for col in self.sorted_features:
             if col not in state_features_df.columns:
@@ -55,39 +70,7 @@ class PandasSparseToDenseProcessor(SparseToDenseProcessor):
         ).float()
         if self.set_missing_value_to_zero:
             # When we set missing values to 0, we don't know what is and isn't missing
-            presence = values != 0.0
+            presence = torch.ones_like(values, dtype=torch.bool)
         else:
             presence = values != missing_value
         return values, presence
-
-
-class PythonSparseToDenseProcessor(SparseToDenseProcessor):
-    def __init__(
-        self, sorted_features: List[int], set_missing_value_to_zero: bool = False
-    ):
-        super().__init__(sorted_features, set_missing_value_to_zero)
-        self.feature_id_to_index: Dict[int, int] = {}
-        for index, feature_id in enumerate(sorted_features):
-            self.feature_id_to_index[feature_id] = index
-
-    def process(
-        self, sparse_data: List[Dict[int, float]]
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        dense_data = torch.ones([len(sparse_data), len(self.feature_id_to_index)])
-        dense_presence = torch.zeros(
-            [len(sparse_data), len(self.feature_id_to_index)]
-        ).byte()
-        for i, feature_map in enumerate(sparse_data):
-            assert (
-                feature_map is not None
-            ), f"Please make sure that features are not NULL; row {i}"
-            for j, value in feature_map.items():
-                j_index = self.feature_id_to_index.get(j, None)
-                if j_index is None:
-                    continue
-                dense_data[i][j_index] = value
-                dense_presence[i][j_index] = 1
-        if self.set_missing_value_to_zero:
-            # When we set missing values to 0, we don't know what is and isn't missing
-            dense_presence = dense_data != 0.0
-        return (dense_data, dense_presence)
