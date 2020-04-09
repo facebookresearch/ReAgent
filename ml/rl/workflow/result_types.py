aee4d09e31be38302c357eae6a6d4d8fd9d3e8ae
Kittipat Virochsiri <kittipat@fb.com>
2020-04-08 18:20:43 -0700

    remove polyfill types
    Summary: deadcode
    
    Reviewed By: kaiwenw
    
    Differential Revision: D20853084
    
    fbshipit-source-id: af30f6c4aa0edf198e2fcc9a74a1759aebb4e475

--- ml/rl/workflow/result_types.py
+++ ml/rl/workflow/result_types.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
-from ml.rl.polyfill.types_lib.union import TaggedUnion
+from ml.rl.core.dataclasses import dataclass
+from ml.rl.core.tagged_union import TaggedUnion
 
 
 class NoPublishingResults(NamedTuple):
@@ -13,11 +14,13 @@ class NoValidationResults(NamedTuple):
     pass
 
 
+@dataclass
 class PublishingResults(TaggedUnion):
-    no_publishing_results: NoPublishingResults
+    no_publishing_results: Optional[NoPublishingResults] = None
     # Add your own validation results type here
 
 
+@dataclass
 class ValidationResults(TaggedUnion):
-    no_validation_results: NoValidationResults
+    no_validation_results: Optional[NoValidationResults] = None
     # Add your own validation results type here
