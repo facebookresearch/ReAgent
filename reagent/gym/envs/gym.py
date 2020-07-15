310a33af6e29de84247dfca6395fed750a20014b
Kaiwen Wang <kaiwenw@fb.com>
2020-07-14 20:09:42 -0700

    Create Env Registry, clean up environments (#289)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/289
    
    Simplify configuration of environments, towards eliminating HAS_RECSIM
    
    Reviewed By: czxttkl
    
    Differential Revision: D22483338
    
    fbshipit-source-id: d66f1cc3ba27c4a3c390d265405483e727b28d03

---
+++ reagent/gym/envs/gym.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import logging
+from typing import Optional
+
+import gym
+from gym_minigrid.wrappers import ReseedWrapper
+from reagent.core.dataclasses import dataclass
+from reagent.gym.envs.env_wrapper import EnvWrapper
+from reagent.gym.envs.wrappers.simple_minigrid import SimpleObsWrapper
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Gym(EnvWrapper):
+    env_name: str
+    max_steps: Optional[int] = None
+
+    def make(self) -> gym.Env:
+        kwargs = {}
+        if self.max_steps is not None:
+            kwargs["max_steps"] = self.max_steps
+        env: gym.Env = gym.make(self.env_name, **kwargs)
+        if self.env_name.startswith("MiniGrid-"):
+            # Wrap in minigrid simplifier
+            env = SimpleObsWrapper(ReseedWrapper(env))
+        return env
