69061e67d62a067c2a8a5c6a440f7b9605c111d6
Badri Narayan Bhaskar <badri@fb.com>
2020-08-28 23:29:28 -0700

    Fix Reinforce Defaults and Off-policy correction
    Reviewed By: kaiwenw, czxttkl
    
    Differential Revision: D23402183
    
    fbshipit-source-id: 445ee41679232921c404c1cc717057f8c5622dd3

--- reagent/training/reinforce.py
+++ reagent/training/reinforce.py
@@ -20,10 +20,12 @@ class ReinforceParams:
     gamma: float = 0.0
     optimizer: Optimizer__Union = field(default_factory=Optimizer__Union.default)
     off_policy: bool = False
+    reward_clip: float = 1e6
     clip_param: float = 1e6
     normalize: bool = True
     subtract_mean: bool = True
-    offset_clamp_min: bool = True
+    offset_clamp_min: bool = False
+    update_freq: int = 1
 
 
 class Reinforce(Trainer):
@@ -32,28 +34,43 @@ class Reinforce(Trainer):
         self.sampler = actor.sampler
         self.params = params
         self.optimizer = params.optimizer.make_optimizer(self.scorer.parameters())
+        self.step = 1
+        self.losses = []
+
+    def update_model(self):
+        if len(self.losses) > 0:
+            self.optimizer.zero_grad()
+            loss = torch.stack(self.losses).mean()
+            loss.backward()
+            del self.losses[:]
+            self.optimizer.step()
 
     def train(self, training_batch: rlt.PolicyGradientInput) -> None:
         actions = training_batch.action
         rewards = training_batch.reward.detach()
         scores = self.scorer(training_batch.state)
         characteristic_eligibility = self.sampler.log_prob(scores, actions).float()
-        offset_reinforcement = discounted_returns(rewards, self.params.gamma)
+        offset_reinforcement = discounted_returns(
+            torch.clamp(rewards, max=self.params.reward_clip).clone(), self.params.gamma
+        )
         if self.params.normalize:
             offset_reinforcement = whiten(
                 offset_reinforcement, subtract_mean=self.params.subtract_mean
             )
         if self.params.offset_clamp_min:
             offset_reinforcement = offset_reinforcement.clamp(min=0)  # pyre-ignore
-        correction = 1.0
         if self.params.off_policy:
-            correction = torch.exp(characteristic_eligibility - training_batch.log_prob)
-            correction *= (correction < self.params.clip_param).float()
-            characteristic_eligibility *= correction.detach()
-        err = -(offset_reinforcement.float()) @ characteristic_eligibility
-        self.optimizer.zero_grad()
-        err.backward()
-        self.optimizer.step()
+            target_propensity = self.sampler.log_prob(scores, actions).float()
+            characteristic_eligibility = torch.exp(
+                torch.clamp(
+                    target_propensity - training_batch.log_prob.detach(),
+                    max=torch.log(self.params.clip_param),
+                )
+            )
+        self.losses.append(-(offset_reinforcement.float()) @ characteristic_eligibility)
+        self.step += 1
+        if self.step % self.params.update_freq == 0:
+            self.update_model()
 
     def warm_start_components(self) -> List[str]:
         """
