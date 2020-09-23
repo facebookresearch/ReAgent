f42595df8d67f6164b779d41fd6be4bb989df9d1
Zhengxing Chen <czxttkl@fb.com>
2020-09-22 20:40:54 -0700

    Add iterative-softmax decoder for Seq2Slate (#316)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/316
    
    We used to only have an autoregressive decoder for seq2slate. In this diff we add an iterative-softmax decoder for seq2slate. It only computes encoder scores once and then perform iterative softmax at each step until the full slate is decoded. It is equivalent to Frechet Policy Gradient mathematically.
    
    We also add and fix more tests in the diff.
    
    Reviewed By: badrinarayan
    
    Differential Revision: D23807407
    
    fbshipit-source-id: 8184fe33d2388e594e056ed7da7ab75cba201c45

--- reagent/net_builder/slate_ranking/slate_ranking_transformer.py
+++ reagent/net_builder/slate_ranking/slate_ranking_transformer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+
 from reagent.core.dataclasses import dataclass, field
+from reagent.model_utils.seq2slate_utils import Seq2SlateOutputArch
 from reagent.models.base import ModelBase
 from reagent.models.seq2slate import Seq2SlateTransformerNet
 from reagent.net_builder.slate_ranking_net_builder import SlateRankingNetBuilder
@@ -11,6 +13,8 @@ from reagent.parameters import TransformerParameters, param_hash
 class SlateRankingTransformer(SlateRankingNetBuilder):
     __hash__ = param_hash
 
+    output_arch: Seq2SlateOutputArch = Seq2SlateOutputArch.AUTOREGRESSIVE
+    temperature: float = 1.0
     transformer: TransformerParameters = field(
         default_factory=lambda: TransformerParameters(
             num_heads=2, dim_model=16, dim_feedforward=16, num_stacked_layers=2
@@ -29,5 +33,6 @@ class SlateRankingTransformer(SlateRankingNetBuilder):
             dim_feedforward=self.transformer.dim_feedforward,
             max_src_seq_len=candidate_size,
             max_tgt_seq_len=slate_size,
-            encoder_only=False,
+            output_arch=self.output_arch,
+            temperature=self.temperature,
         )
