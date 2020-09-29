2b8d6814a4029d12f3e4e71d589a7b425b676ec8
Kittipat Virochsiri <kittipat@fb.com>
2020-09-29 11:50:22 -0700

    Create ObservableMixin
    Summary: Prep for migrating to Lightning
    
    Reviewed By: czxttkl
    
    Differential Revision: D23946582
    
    fbshipit-source-id: bc9a43318725b1c510c8c4cf71c391d3715d3908

--- reagent/core/tracker.py
+++ reagent/core/tracker.py
@@ -3,7 +3,7 @@
 
 import functools
 import logging
-from typing import List
+from typing import Dict, List, Type
 
 import torch
 
@@ -41,6 +41,52 @@ class Aggregator:
         pass
 
 
+class ObservableMixin:
+    def __init__(self):
+        super().__init__()
+        self._observers = {v: [] for v in self._observable_value_types}
+
+    @property
+    def _observable_value_types(self) -> Dict[str, Type]:
+        raise NotImplementedError
+
+    def add_observer(self, observer: Observer):
+        observing_keys = observer.get_observing_keys()
+        unknown_keys = [
+            k for k in observing_keys if k not in self._observable_value_types
+        ]
+        if unknown_keys:
+            logger.warning(f"{unknown_keys} cannot be observed in {type(self)}")
+        for k in observing_keys:
+            if k in self._observers and observer not in self._observers[k]:
+                self._observers[k].append(observer)
+        return self
+
+    def add_observers(self, observers: List[Observer]):
+        for observer in observers:
+            self.add_observer(observer)
+        return self
+
+    def notify_observers(self, **kwargs):
+        for key, value in kwargs.items():
+            if value is None:
+                # Allow optional reporting
+                continue
+
+            assert key in self._observers, f"Unknown key: {key}"
+
+            # TODO: Create a generic framework for type conversion
+            if self._observable_value_types[key] == torch.Tensor:
+                if not isinstance(value, torch.Tensor):
+                    value = torch.tensor(value)
+                if len(value.shape) == 0:
+                    value = value.reshape(1)
+                value = value.detach()
+
+            for observer in self._observers[key]:
+                observer.update(key, value)
+
+
 def observable(cls=None, **kwargs):  # noqa: C901
     """
     Decorator to mark a class as producing observable values. The names of the
@@ -67,47 +113,11 @@ def observable(cls=None, **kwargs):  # noqa: C901
 
         cls.__init__ = new_init
 
-        def add_observer(self, observer: Observer) -> None:
-            observing_keys = observer.get_observing_keys()
-            unknown_keys = [
-                k for k in observing_keys if k not in self._observable_value_types
-            ]
-            if unknown_keys:
-                logger.warning(f"{unknown_keys} cannot be observed in {type(self)}")
-            for k in observing_keys:
-                if k in self._observers and observer not in self._observers[k]:
-                    self._observers[k].append(observer)
-            return self
+        cls.add_observer = ObservableMixin.add_observer
 
-        cls.add_observer = add_observer
+        cls.add_observers = ObservableMixin.add_observers
 
-        def add_observers(self, observers: List[Observer]) -> None:
-            for observer in observers:
-                self.add_observer(observer)
-            return self
-
-        cls.add_observers = add_observers
-
-        def notify_observers(self, **kwargs):
-            for key, value in kwargs.items():
-                if value is None:
-                    # Allow optional reporting
-                    continue
-
-                assert key in self._observers, f"Unknown key: {key}"
-
-                # TODO: Create a generic framework for type conversion
-                if self._observable_value_types[key] == torch.Tensor:
-                    if not isinstance(value, torch.Tensor):
-                        value = torch.tensor(value)
-                    if len(value.shape) == 0:
-                        value = value.reshape(1)
-                    value = value.detach()
-
-                for observer in self._observers[key]:
-                    observer.update(key, value)
-
-        cls.notify_observers = notify_observers
+        cls.notify_observers = ObservableMixin.notify_observers
 
         return cls
 
