19d961ab735c6c649e0bc9acc8439122e4c490ba
Ruiyang Xu <ruiyangxu@fb.com>
2020-10-15 01:43:10 -0700

    Add plots for compress model
    Summary: Add plots for compress model
    
    Reviewed By: czxttkl
    
    Differential Revision: D24327103
    
    fbshipit-source-id: 40b9e6aba3199274fc1bb57f2fec847348055c70

--- reagent/evaluation/compress_model_evaluator.py
+++ reagent/evaluation/compress_model_evaluator.py
@@ -4,6 +4,7 @@ import logging
 
 import torch
 from reagent.training.world_model.compress_model_trainer import CompressModelTrainer
+from reagent.training.world_model.seq2reward_trainer import get_Q
 from reagent.types import MemoryNetworkInput
 
 
@@ -18,10 +19,26 @@ class CompressModelEvaluator:
     # pyre-fixme[56]: Decorator `torch.no_grad(...)` could not be called, because
     #  its type `no_grad` is not callable.
     @torch.no_grad()
-    def evaluate(self, eval_tdp: MemoryNetworkInput):
+    def evaluate(self, eval_batch: MemoryNetworkInput):
         prev_mode = self.compress_model_network.training
         self.compress_model_network.eval()
-        loss = self.trainer.get_loss(eval_tdp)
+        loss = self.trainer.get_loss(eval_batch)
         detached_loss = loss.cpu().detach().item()
+
+        # shape: batch_size, action_dim
+        q_values_all_action_all_data = get_Q(
+            self.trainer.seq2reward_network, eval_batch, self.trainer.all_permut
+        ).cpu()
+        q_values = q_values_all_action_all_data.mean(0).tolist()
+
+        action_distribution = torch.bincount(
+            torch.argmax(q_values_all_action_all_data, dim=1),
+            minlength=len(self.trainer.params.action_names),
+        )
+        # normalize
+        action_distribution = (
+            action_distribution.float() / torch.sum(action_distribution)
+        ).tolist()
+
         self.compress_model_network.train(prev_mode)
-        return detached_loss
+        return (detached_loss, q_values, action_distribution)
