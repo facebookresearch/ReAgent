3ebdc52afb02e899374fd363cc30fdc8a6a329aa
Kittipat Virochsiri <kittipat@fb.com>
2020-12-01 15:34:48 -0800

    Simplifying random agent creation
    Summary: One less thing to import
    
    Reviewed By: czxttkl
    
    Differential Revision: D24576862
    
    fbshipit-source-id: 9548dcb87e85f244b816ed808c4959fdd251e370

--- reagent/gym/__init__.py
+++ reagent/gym/__init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
+from .agents.agent import Agent
+from .envs.gym import Gym
+
+
+__all__ = ["Agent", "Gym"]
