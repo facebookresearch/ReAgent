e9da416b1c8bcc4c6c9cc8b7d2df8a5713af662b
Joel Schlosser <jbschlosser@fb.com>
2020-12-02 06:56:36 -0800

    Fix LR schedulers in ReAgent + supporting tests (#344)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/344
    
    Fixed LR schedulers in ReAgent by adding a "verbose" field to each to match their torch.optim counterparts.
    
    To test this, a new `optimizer_tests` target has been added to ensure that a valid Optimizer can be created for each type of LR scheduler (excluding LambdaLR, MultiplicativeLR, and CyclicLR for now as they are not supported yet).
    
    Reviewed By: alexnikulkov
    
    Differential Revision: D24841422
    
    fbshipit-source-id: 6be13a92d7206aa693b0068f10744aeb3d4efb4a

---
+++ reagent/test/optimizer/test_make_optimizer.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
+from reagent.optimizer.uninferrable_optimizers import Adam
+from reagent.optimizer.uninferrable_schedulers import (
+    CosineAnnealingLR,
+    CosineAnnealingWarmRestarts,
+    ExponentialLR,
+    MultiStepLR,
+    OneCycleLR,
+    StepLR,
+)
+from reagent.optimizer.utils import is_torch_lr_scheduler, is_torch_optimizer
+import torch
+import unittest
+
+
+class TestMakeOptimizer(unittest.TestCase):
+    def setUp(self):
+        self.model = torch.nn.Linear(3, 4)
+
+    def _verify_optimizer(self, optimizer):
+        self.assertTrue(is_torch_optimizer(type(optimizer.optimizer)))
+        for lr_scheduler in optimizer.lr_schedulers:
+            self.assertTrue(is_torch_lr_scheduler(type(lr_scheduler)))
+
+    def test_make_optimizer_with_step_lr_scheduler(self):
+        self._verify_optimizer(
+            Adam(
+                lr=0.001, lr_schedulers=[StepLR(gamma=0.1, step_size=0.01)]
+            ).make_optimizer(self.model.parameters())
+        )
+
+    def test_make_optimizer_with_multistep_lr_scheduler(self):
+        self._verify_optimizer(
+            Adam(
+                lr=0.001,
+                lr_schedulers=[MultiStepLR(gamma=0.2, milestones=[1000, 2000])],
+            ).make_optimizer(self.model.parameters())
+        )
+
+    def test_make_optimizer_with_exponential_lr_scheduler(self):
+        self._verify_optimizer(
+            Adam(lr=0.001, lr_schedulers=[ExponentialLR(gamma=0.9)]).make_optimizer(
+                self.model.parameters()
+            )
+        )
+
+    def test_make_optimizer_with_cosine_annealing_lr_scheduler(self):
+        self._verify_optimizer(
+            Adam(lr=0.001, lr_schedulers=[CosineAnnealingLR(T_max=1)]).make_optimizer(
+                self.model.parameters()
+            )
+        )
+
+    def test_make_optimizer_with_one_cycle_lr_scheduler(self):
+        self._verify_optimizer(
+            Adam(
+                lr=0.001,
+                lr_schedulers=[
+                    OneCycleLR(max_lr=0.1, base_momentum=0.8, total_steps=1000)
+                ],
+            ).make_optimizer(self.model.parameters())
+        )
+
+    def test_make_optimizer_with_cosine_annealing_warm_restarts_lr_scheduler(self):
+        self._verify_optimizer(
+            Adam(
+                lr=0.001, lr_schedulers=[CosineAnnealingWarmRestarts(T_0=1)]
+            ).make_optimizer(self.model.parameters())
+        )
