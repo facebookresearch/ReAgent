e381209349ccbdc8bc27ae3834c7f67d06fcbc61
Kittipat Virochsiri <kittipat@fb.com>
2021-01-22 12:13:20 -0800

    Manual DataModule (#350)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/350
    
    First step to simplify ModelManager; separating out data handling into the data module.
    
    Reviewed By: czxttkl
    
    Differential Revision: D25377364
    
    fbshipit-source-id: 2924a80b870b507e63f4bd85f407119a6c2cd8b0

--- reagent/workflow/data/__init__.py
+++ reagent/workflow/data/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
+from .manual_data_module import ManualDataModule
 from .reagent_data_module import ReAgentDataModule
 
 
-__all__ = ["ReAgentDataModule"]
+__all__ = ["ReAgentDataModule", "ManualDataModule"]
