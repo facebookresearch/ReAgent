8bbb8af70eec582e97bd04d839f14baba6cdfdb3
Kittipat Virochsiri <kittipat@fb.com>
2021-02-12 16:57:27 -0800

    Fix BaseDataClass (#395)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/395
    
    There is some inheritance problem when `BaseDataClass` is a standard dataclass but the subclass is a pydantic dataclass. Since `BaseDataClass` doesn't have its own field, it doesn't need to be a dataclass.
    
    Reviewed By: czxttkl
    
    Differential Revision: D26434426
    
    fbshipit-source-id: 1517f7e68541912f017dbd48b7ea05f95537868c

--- reagent/base_dataclass.py
+++ reagent/base_dataclass.py
@@ -5,11 +5,9 @@
 We should revisit this at some point. Config classes shouldn't subclass from this.
 """
 import dataclasses
-from dataclasses import dataclass
 from typing import cast
 
 
-@dataclass
 class BaseDataClass:
     def _replace(self, **kwargs):
         return cast(type(self), dataclasses.replace(self, **kwargs))
