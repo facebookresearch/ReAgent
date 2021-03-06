8f4e55aa72dd6959ba19a88b14c4e518d3bd3e98
Jason Gauci <jjg@fb.com>
2021-03-05 23:11:05 -0800

    Deprecate value_list_observer (#408)
    Summary: Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/408
    
    Reviewed By: czxttkl
    
    Differential Revision: D26635649
    
    fbshipit-source-id: 9d6a3aa554dfa91b431c9e9e6785625f71c2ae66

--- reagent/workflow/reporters/discrete_dqn_reporter.py
+++ reagent/workflow/reporters/discrete_dqn_reporter.py
@@ -26,76 +26,82 @@ class DiscreteDQNReporter(FlexibleDataPointsPerEpochMixin, ReporterBase):
         target_action_distribution: Optional[List[float]] = None,
         recent_window_size: int = 100,
     ):
-        self.value_list_observers = {"cpe_results": ValueListObserver("cpe_details")}
-        self.aggregating_observers = OrderedDict(
-            (name, IntervalAggregatingObserver(report_interval, aggregator))
-            for name, aggregator in itertools.chain(
-                [
-                    ("td_loss", agg.MeanAggregator("td_loss")),
-                    ("reward_loss", agg.MeanAggregator("reward_loss")),
-                    (
-                        "model_values",
-                        agg.FunctionsByActionAggregator(
+        self.value_list_observers = {}
+        self.aggregating_observers = {
+            **{
+                "cpe_results": IntervalAggregatingObserver(
+                    1, agg.ListAggregator("cpe_details")
+                ),
+            },
+            **{
+                name: IntervalAggregatingObserver(report_interval, aggregator)
+                for name, aggregator in itertools.chain(
+                    [
+                        ("td_loss", agg.MeanAggregator("td_loss")),
+                        ("reward_loss", agg.MeanAggregator("reward_loss")),
+                        (
                             "model_values",
-                            actions,
-                            {"mean": torch.mean, "std": torch.std},
+                            agg.FunctionsByActionAggregator(
+                                "model_values",
+                                actions,
+                                {"mean": torch.mean, "std": torch.std},
+                            ),
                         ),
-                    ),
-                    (
-                        "logged_action",
-                        agg.ActionCountAggregator("logged_actions", actions),
-                    ),
-                    (
-                        "model_action",
-                        agg.ActionCountAggregator("model_action_idxs", actions),
-                    ),
-                    (
-                        "recent_rewards",
-                        agg.RecentValuesAggregator("logged_rewards"),
-                    ),
-                ],
-                [
-                    (
-                        f"{key}_tb",
-                        agg.TensorBoardActionCountAggregator(key, title, actions),
-                    )
-                    for key, title in [
-                        ("logged_actions", "logged"),
-                        ("model_action_idxs", "model"),
-                    ]
-                ],
-                [
-                    (
-                        f"{key}_tb",
-                        agg.TensorBoardHistogramAndMeanAggregator(key, log_key),
-                    )
-                    for key, log_key in [
-                        ("td_loss", "td_loss"),
-                        ("reward_loss", "reward_loss"),
-                        ("logged_propensities", "propensities/logged"),
-                        ("logged_rewards", "reward/logged"),
-                    ]
-                ],
-                [
-                    (
-                        f"{key}_tb",
-                        agg.TensorBoardActionHistogramAndMeanAggregator(
-                            key, category, title, actions
+                        (
+                            "logged_action",
+                            agg.ActionCountAggregator("logged_actions", actions),
                         ),
-                    )
-                    for key, category, title in [
-                        ("model_propensities", "propensities", "model"),
-                        ("model_rewards", "reward", "model"),
-                        ("model_values", "value", "model"),
-                    ]
-                ],
-            )
-        )
+                        (
+                            "model_action",
+                            agg.ActionCountAggregator("model_action_idxs", actions),
+                        ),
+                        (
+                            "recent_rewards",
+                            agg.RecentValuesAggregator("logged_rewards"),
+                        ),
+                    ],
+                    [
+                        (
+                            f"{key}_tb",
+                            agg.TensorBoardActionCountAggregator(key, title, actions),
+                        )
+                        for key, title in [
+                            ("logged_actions", "logged"),
+                            ("model_action_idxs", "model"),
+                        ]
+                    ],
+                    [
+                        (
+                            f"{key}_tb",
+                            agg.TensorBoardHistogramAndMeanAggregator(key, log_key),
+                        )
+                        for key, log_key in [
+                            ("td_loss", "td_loss"),
+                            ("reward_loss", "reward_loss"),
+                            ("logged_propensities", "propensities/logged"),
+                            ("logged_rewards", "reward/logged"),
+                        ]
+                    ],
+                    [
+                        (
+                            f"{key}_tb",
+                            agg.TensorBoardActionHistogramAndMeanAggregator(
+                                key, category, title, actions
+                            ),
+                        )
+                        for key, category, title in [
+                            ("model_propensities", "propensities", "model"),
+                            ("model_rewards", "reward", "model"),
+                            ("model_values", "value", "model"),
+                        ]
+                    ],
+                )
+            },
+        }
         super().__init__(self.value_list_observers, self.aggregating_observers)
         self.target_action_distribution = target_action_distribution
         self.recent_window_size = recent_window_size
 
     # TODO: write this for OSS
     def generate_training_report(self) -> DQNTrainingReport:
-        cpe_results = self.value_list_observers["cpe_results"].values  # noqa
         return DQNTrainingReport()
--- reagent/workflow/reporters/parametric_dqn_reporter.py
+++ reagent/workflow/reporters/parametric_dqn_reporter.py
@@ -15,33 +15,43 @@ logger = logging.getLogger(__name__)
 
 class ParametricDQNReporter(ReporterBase):
     def __init__(self, report_interval: int = 100):
-        self.value_list_observers = {"cpe_results": ValueListObserver("cpe_details")}
-        self.aggregating_observers = OrderedDict(
-            (name, IntervalAggregatingObserver(report_interval, aggregator))
-            for name, aggregator in itertools.chain(
-                [
-                    ("td_loss", agg.MeanAggregator("td_loss")),
-                    ("reward_loss", agg.MeanAggregator("reward_loss")),
-                    ("recent_rewards", agg.RecentValuesAggregator("logged_rewards")),
-                    (
-                        "model_values_on_logged_actions",
-                        agg.MeanAggregator("model_values_on_logged_actions"),
-                    ),
-                ],
-                [
-                    (
-                        f"{key}_tb",
-                        agg.TensorBoardHistogramAndMeanAggregator(key, log_key),
-                    )
-                    for key, log_key in [
-                        ("td_loss", "td_loss"),
-                        ("reward_loss", "reward_loss"),
-                        ("logged_propensities", "propensities/logged"),
-                        ("logged_rewards", "reward/logged"),
-                    ]
-                ],
-            )
-        )
+        self.value_list_observers = {}
+        self.aggregating_observers = {
+            **{
+                "cpe_results": IntervalAggregatingObserver(
+                    1, agg.ListAggregator("cpe_details")
+                ),
+            },
+            **{
+                name: IntervalAggregatingObserver(report_interval, aggregator)
+                for name, aggregator in itertools.chain(
+                    [
+                        ("td_loss", agg.MeanAggregator("td_loss")),
+                        ("reward_loss", agg.MeanAggregator("reward_loss")),
+                        (
+                            "recent_rewards",
+                            agg.RecentValuesAggregator("logged_rewards"),
+                        ),
+                        (
+                            "model_values_on_logged_actions",
+                            agg.MeanAggregator("model_values_on_logged_actions"),
+                        ),
+                    ],
+                    [
+                        (
+                            f"{key}_tb",
+                            agg.TensorBoardHistogramAndMeanAggregator(key, log_key),
+                        )
+                        for key, log_key in [
+                            ("td_loss", "td_loss"),
+                            ("reward_loss", "reward_loss"),
+                            ("logged_propensities", "propensities/logged"),
+                            ("logged_rewards", "reward/logged"),
+                        ]
+                    ],
+                )
+            },
+        }
         super().__init__(self.value_list_observers, self.aggregating_observers)
 
     # TODO: write this for OSS
