787ae61cf0479fd17b3296864406491b25789c35
Jason Gauci <jjg@fb.com>
2021-03-11 19:30:43 -0800

    Unify DQNTrainingReport (#413)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/413
    
    This merges DQNTrainingReport and most of DiscreteDQNReporter.  Once D26626393 has landed, we can also merge DiscreteDQNReporter.
    
    Reviewed By: czxttkl
    
    Differential Revision: D26896744
    
    fbshipit-source-id: 5db1992997a594eaf07f60dc8832349e17bd2d13

--- reagent/core/result_registries.py
+++ reagent/core/result_registries.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
 
+from dataclasses import field
+from typing import List, Any
+
 from reagent.core.dataclasses import dataclass
 from reagent.core.registry_meta import RegistryMeta
 
 
+@dataclass
 class TrainingReport(metaclass=RegistryMeta):
-    pass
+    plots: List[Any] = field(default_factory=list)
 
 
 @dataclass
