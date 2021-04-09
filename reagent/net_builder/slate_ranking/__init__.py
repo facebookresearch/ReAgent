aa3328049d9fa2f939159c0724b3269d0c8c2a67
Kaiwen Wang <kaiwenw@fb.com>
2021-04-08 23:01:10 -0700

    Train Reels LearnedVM as a residual boost
    Summary:
    - add option to train as residual boost (on top of prod vm score)
    - net builder for MLP for better configuration of MLPScorer
    - filter out slates with 0 scores (ostensibly from precision problems); these caused nan problems in training
    - add option for orthogonal weight initialization
    
    Reviewed By: czxttkl
    
    Differential Revision: D27264221
    
    fbshipit-source-id: 96663cf3b18be96c1885126ad3df50f41b49d778

--- reagent/net_builder/slate_ranking/__init__.py
+++ reagent/net_builder/slate_ranking/__init__.py
@@ -3,6 +3,7 @@
 from reagent.core.tagged_union import TaggedUnion
 from reagent.net_builder.slate_ranking_net_builder import SlateRankingNetBuilder
 
+from . import slate_ranking_scorer  # noqa
 from . import slate_ranking_transformer  # noqa
 
 
