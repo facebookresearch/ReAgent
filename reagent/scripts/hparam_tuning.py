5eda5d7a9d00a3526aa66cb5a54eb98e0a6954dc
Alex Nikulkov <alexnik@fb.com>
2021-04-23 10:00:16 -0700

    Add logging of all considered parameters to hyperparam tuning script
    Summary:
    - Add more elements to the output
    - Fix dependency in TARGETS
    - Fix some typos in comments
    - Wrap paths in `os.path.expanduser()`
    
    Reviewed By: bankawas
    
    Differential Revision: D27946814
    
    fbshipit-source-id: b9cd0bedfecc1e63007e7d15f40a5431ed85e3ae

--- reagent/scripts/hparam_tuning.py
+++ reagent/scripts/hparam_tuning.py
@@ -31,7 +31,7 @@ def ax_evaluate_params(
         fixed_params: A dictionary of hyperparameters that are held fixed between evaluations.
         eval_fn: Evaluation function that returns a dictionary of metric values.
         parse_params_fn: A optional function applied to the hyperparameter dictionary to parse some elements. Can be useful
-            if the best represenation for Ax doesn't match the format accepted by the eval_fn.
+            if the best representation for Ax doesn't match the format accepted by the eval_fn.
         num_seeds: Number of random seeds among which the metrics are averaged.
         num_proc: Number of processes to run in parallel.
     Returns:
@@ -101,11 +101,11 @@ def run_ax_search(
         fixed_params: Fixed values of hyperparameters.
         ax_params: Ax configuration for hyperparameters that are searched over. See docs for ax_client.create_experiment()
         eval_fn: Evaluation function that returns a dictionary of metric values.
-        obj_name: Objective name (key of the dict reterned by eval_fn)
+        obj_name: Objective name (key of the dict returned by eval_fn)
         minimize: If True, objective is minimized, if False it's maximized.
         id_: An arbitrary string identifier of the search (used as part of filename where results are saved)
         parse_params_fn: A function applied to the parameter dictionary to parse it. Can be used
-            if the best represenation for Ax doesn't match the format accepted by the eval_fn.
+            if the best representation for Ax doesn't match the format accepted by the eval_fn.
         ax_param_constraints: Constraints for the parameters that are searched over.
         num_ax_steps: The number of ax steps to take.
         num_concur_samples: Number of configurations to sample per ax step (in parallel)
@@ -135,6 +135,8 @@ def run_ax_search(
         },
     )
     best_params = None
+    all_considered_params = []
+    all_considered_metrics = []
 
     try:
         for i in range(1, num_ax_steps + 1):
@@ -155,6 +157,8 @@ def run_ax_search(
                 num_seeds=num_seeds,
                 num_proc=num_proc,
             )
+            all_considered_params.extend(params_list)
+            all_considered_metrics.extend(res)
             for t_i, v in zip(trial_indices_list, res):
                 ax_client.complete_trial(trial_index=t_i, raw_data=v)
             best_params, predicted_metrics = ax_client.get_best_parameters()
@@ -165,16 +169,23 @@ def run_ax_search(
             if folder_name is not None:
                 with open(
                     os.path.join(
-                        folder_name,
+                        os.path.expanduser(folder_name),
                         f"ax_results_{id_}.json",
                     ),
                     "w",
                 ) as f:
                     json.dump(
                         {
-                            "fixed_params": fixed_params,
                             "best_params": best_params,
                             "predicted_metrics": predicted_metrics,
+                            "fixed_params": fixed_params,
+                            "ax_params": ax_params,
+                            "num_ax_steps": i,
+                            "num_concur_samples": num_concur_samples,
+                            "num_seeds": num_seeds,
+                            "num_proc": num_proc,
+                            "all_considered_params": all_considered_params,
+                            "all_considered_metrics": all_considered_metrics,
                         },
                         f,
                         indent=4,
