08cb1a2a104192510746860ea20054e3134b69c0
Zhengxing Chen <czxttkl@fb.com>
2021-05-11 06:25:49 -0700

    Force specifying rerank_topk in internal diversity predictor (#473)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/473
    
    To satisfy a client team's request
    
    Differential Revision: D28327536
    
    fbshipit-source-id: d3b1f9ef0c6b6bc09b29930d59ed2834cdadd7df

--- reagent/prediction/ranking/predictor_wrapper.py
+++ reagent/prediction/ranking/predictor_wrapper.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List
+from typing import Tuple, List, Optional
 
 import torch
 import torch.nn.functional as F
@@ -7,12 +7,18 @@ import torch.nn.functional as F
 class DeterminantalPointProcessPredictorWrapper(torch.jit.ScriptModule):
     """http://jgillenw.com/cikm2018.pdf Algorithm 1"""
 
-    def __init__(self, alpha) -> None:
+    def __init__(self, alpha, rerank_topk: Optional[int] = None) -> None:
         super().__init__()
         # control the strength of encouragement for diversity
         self.alpha = alpha
         # hard code this value so jit.script can work
         self.MIN_VALUE = -3.4e38
+        # if None, will rerank the full slate
+        self.rerank_topk = rerank_topk
+        if self.rerank_topk is not None:
+            # pyre-fixme[58]: `>` is not supported for operand types `Optional[int]`
+            #  and `int`.
+            assert self.rerank_topk > 0
 
     def unchosen_dets(self, L, chosen: List[int]):
         slate_size = L.shape[0]
@@ -26,11 +32,25 @@ class DeterminantalPointProcessPredictorWrapper(torch.jit.ScriptModule):
         slate_size = L.shape[0]
         dets = torch.zeros(slate_size, slate_size, device=L.device)
         chosen: List[int] = []
-        for i in range(slate_size):
+        unchosen = torch.ones(slate_size)
+
+        if self.rerank_topk is not None:
+            rerank_topk = min(self.rerank_topk, slate_size)
+        else:
+            rerank_topk = slate_size
+
+        for i in range(rerank_topk):
             unchosen_dets = self.unchosen_dets(L, chosen)
             dets[i, :] = unchosen_dets
-            chosen.append(torch.argmax(unchosen_dets).item())
-        return torch.tensor(chosen), dets
+            chosen_idx = torch.argmax(unchosen_dets)
+            chosen.append(chosen_idx.item())
+            unchosen[chosen_idx] = 0
+
+        final_order = torch.tensor(chosen)
+        if rerank_topk != slate_size:
+            final_order = torch.cat((final_order, torch.nonzero(unchosen).flatten()))
+
+        return final_order, dets
 
     @torch.jit.script_method
     def forward(
