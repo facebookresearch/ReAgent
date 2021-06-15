d37e430b2e684b5575b9193b267ed119b691a053
Geng Ji <gji@fb.com>
2021-06-14 18:10:59 -0700

    Model manager for training contextual-bandit reward models
    Summary: A standalone workflow to train reward models for discrete-action contextual bandit problems.
    
    Reviewed By: kittipatv
    
    Differential Revision: D28937902
    
    fbshipit-source-id: 5d7f21f0e462e25ba23defe65ff79164e10d65bf

--- reagent/training/cfeval/bandit_reward_network_trainer.py
+++ reagent/training/cfeval/bandit_reward_network_trainer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
 import logging
-from enum import Enum
 from typing import Optional
 
 import numpy as np
@@ -11,55 +11,12 @@ from reagent.core.dataclasses import field
 from reagent.models.base import ModelBase
 from reagent.optimizer.union import Optimizer__Union
 from reagent.training.reagent_lightning_module import ReAgentLightningModule
-
+from reagent.training.reward_network_trainer import _get_loss_function, LossFunction
 
 logger = logging.getLogger(__name__)
 
 
-class LossFunction(Enum):
-    MSE = "MSE_Loss"
-    SmoothL1Loss = "SmoothL1_Loss"
-    L1Loss = "L1_Loss"
-    BCELoss = "BCE_Loss"
-
-
-def _get_loss_function(
-    loss_fn: LossFunction,
-    reward_ignore_threshold: Optional[float],
-    weighted_by_inverse_propensity: bool,
-):
-    reduction_type = "none"
-
-    if loss_fn == LossFunction.MSE:
-        torch_fn = torch.nn.MSELoss(reduction=reduction_type)
-    elif loss_fn == LossFunction.SmoothL1Loss:
-        torch_fn = torch.nn.SmoothL1Loss(reduction=reduction_type)
-    elif loss_fn == LossFunction.L1Loss:
-        torch_fn = torch.nn.L1Loss(reduction=reduction_type)
-    elif loss_fn == LossFunction.BCELoss:
-        torch_fn = torch.nn.BCELoss(reduction=reduction_type)
-
-    def wrapper_loss_fn(pred, target, weight):
-        loss = torch_fn(pred, target)
-
-        if weighted_by_inverse_propensity:
-            assert weight.shape == loss.shape
-            loss = loss * weight
-
-        # ignore abnormal reward only during training
-        if pred.requires_grad and reward_ignore_threshold is not None:
-            loss = loss[target <= reward_ignore_threshold]
-            assert len(loss) > 0, (
-                f"reward ignore threshold set too small. target={target}, "
-                f"threshold={reward_ignore_threshold}"
-            )
-
-        return torch.mean(loss)
-
-    return wrapper_loss_fn
-
-
-class RewardNetTrainer(ReAgentLightningModule):
+class BanditRewardNetTrainer(ReAgentLightningModule):
     def __init__(
         self,
         reward_net: ModelBase,
@@ -87,27 +44,20 @@ class RewardNetTrainer(ReAgentLightningModule):
         )
         return optimizers
 
-    def _get_sample_weight(self, batch: rlt.PreprocessedRankingInput):
+    def _get_sample_weight(self, batch: rlt.BanditRewardModelInput):
         weight = None
         if self.weighted_by_inverse_propensity:
-            if isinstance(batch, rlt.PreprocessedRankingInput):
-                assert batch.tgt_out_probs is not None
-                # pyre-fixme[58]: `/` is not supported for operand types `float` and
-                #  `Optional[torch.Tensor]`.
-                weight = 1.0 / batch.tgt_out_probs
-            else:
-                raise NotImplementedError(
-                    f"Sampling weighting not implemented for {type(batch)}"
-                )
+            assert batch.action_prob is not None
+            # pyre-fixme[58]: `/` is not supported for operand types `float` and
+            #  `Optional[torch.Tensor]`.
+            weight = 1.0 / batch.action_prob
         return weight
 
-    def _get_target_reward(self, batch: rlt.PreprocessedRankingInput):
-        if isinstance(batch, rlt.PreprocessedRankingInput):
-            target_reward = batch.slate_reward
-        else:
-            target_reward = batch.reward
-        assert target_reward is not None
-        return target_reward
+    def _get_predicted_reward(self, batch: rlt.BanditRewardModelInput):
+        model_rewards_all_actions = self.reward_net(batch.state)
+        logged_action_idxs = torch.argmax(batch.action, dim=1, keepdim=True)
+        predicted_reward = model_rewards_all_actions.gather(1, logged_action_idxs)
+        return predicted_reward
 
     # pyre-fixme[56]: Decorator `torch.no_grad(...)` could not be called, because
     #  its type `no_grad` is not callable.
@@ -120,11 +70,11 @@ class RewardNetTrainer(ReAgentLightningModule):
         )
 
     def train_step_gen(
-        self, training_batch: rlt.PreprocessedRankingInput, batch_idx: int
+        self, training_batch: rlt.BanditRewardModelInput, batch_idx: int
     ):
         weight = self._get_sample_weight(training_batch)
-        target_reward = self._get_target_reward(training_batch)
-        predicted_reward = self.reward_net(training_batch).predicted_reward
+        target_reward = training_batch.reward
+        predicted_reward = self._get_predicted_reward(training_batch)
 
         assert (
             predicted_reward.shape == target_reward.shape
@@ -151,11 +101,14 @@ class RewardNetTrainer(ReAgentLightningModule):
         yield loss
 
     # pyre-ignore inconsistent override because lightning doesn't use types
-    def validation_step(self, batch: rlt.PreprocessedRankingInput, batch_idx: int):
-        reward = self._get_target_reward(batch)
+    def validation_step(self, batch: rlt.BanditRewardModelInput, batch_idx: int):
+        if self._training_batch_type and isinstance(batch, dict):
+            batch = self._training_batch_type.from_dict(batch)
+
+        reward = batch.reward
         self.reporter.log(eval_rewards=reward.flatten().detach().cpu())
 
-        pred_reward = self.reward_net(batch).predicted_reward
+        pred_reward = self._get_predicted_reward(batch)
         self.reporter.log(eval_pred_rewards=pred_reward.flatten().detach().cpu())
 
         weight = self._get_sample_weight(batch)
@@ -172,6 +125,3 @@ class RewardNetTrainer(ReAgentLightningModule):
 
     def validation_epoch_end(self, outputs):
         self.reporter.update_best_model(np.mean(outputs), self.reward_net)
-
-    def warm_start_components(self):
-        return ["reward_net"]
