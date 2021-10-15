5f8233c283c60500585ae5ce76f84ef0eaa640c1
alexnik <>
2021-10-15 10:48:44 -0700

    Add Thompson Sampling to ReAgent MAB and refactor the UCB classes and methods to unify
    Differential Revision: D31642370
    
    fbshipit-source-id: cc279ab28bae9f9f39a857a6b859f99c0b734162

--- reagent/mab/ucb.py
+++ reagent/mab/ucb.py
@@ -3,136 +3,14 @@ from abc import ABC, abstractmethod
 from typing import Union, Optional, List
 
 import torch
+from reagent.mab.mab_algorithm import MABAlgo
 from torch import Tensor
 
 
-def _get_arm_indices(
-    ids_of_all_arms: List[Union[str, int]], ids_of_arms_in_batch: List[Union[str, int]]
-) -> List[int]:
-    arm_idxs = []
-    for i in ids_of_arms_in_batch:
-        try:
-            arm_idxs.append(ids_of_all_arms.index(i))
-        except ValueError:
-            raise ValueError(f"Unknown arm_id {i}. Known arm ids: {ids_of_all_arms}")
-    return arm_idxs
-
-
-def _place_values_at_indices(values: Tensor, idxs: List[int], total_len: int) -> Tensor:
-    """
-
-    TODO: maybe replace with sparse vector function?
-
-    Args:
-        values (Tensor): The values
-        idxs (List[int]): The indices at which the values have to be placed
-        total_len (int): Length of the array
-    """
-    assert len(values) == len(idxs)
-    ret = torch.zeros(total_len)
-    ret[idxs] = values
-    return ret
-
-
-class BaseUCB(torch.nn.Module, ABC):
+class BaseUCB(MABAlgo, ABC):
     """
     Base class for UCB-like Multi-Armed Bandits (MAB)
     """
-
-    def __init__(
-        self,
-        *,
-        n_arms: Optional[int] = None,
-        arm_ids: Optional[List[Union[str, int]]] = None,
-    ):
-        super().__init__()
-        if n_arms is not None:
-            self.arm_ids = list(range(n_arms))
-            self.n_arms = n_arms
-        if arm_ids is not None:
-            self.arm_ids = arm_ids
-            self.n_arms = len(arm_ids)
-        self.total_n_obs_all_arms = 0
-        self.total_n_obs_per_arm = torch.zeros(self.n_arms)
-        self.total_sum_reward_per_arm = torch.zeros(self.n_arms)
-
-    def add_batch_observations(
-        self,
-        n_obs_per_arm: Tensor,
-        sum_reward_per_arm: Tensor,
-        arm_ids: Optional[List[Union[str, int]]] = None,
-    ):
-        if not isinstance(n_obs_per_arm, Tensor):
-            n_obs_per_arm = torch.tensor(n_obs_per_arm, dtype=torch.float)
-        if not isinstance(sum_reward_per_arm, Tensor):
-            sum_reward_per_arm = torch.tensor(sum_reward_per_arm, dtype=torch.float)
-        if arm_ids is None or arm_ids == self.arm_ids:
-            # assume that the observations are for all arms in the default order
-            arm_ids = self.arm_ids
-            arm_idxs = list(range(self.n_arms))
-        else:
-            assert len(arm_ids) == len(
-                set(arm_ids)
-            )  # make sure no duplicates in arm IDs
-
-            # get the indices of the arms
-            arm_idxs = _get_arm_indices(self.arm_ids, arm_ids)
-
-            # put elements from the batch in the positions specified by `arm_ids` (missing arms will be zero)
-            n_obs_per_arm = _place_values_at_indices(
-                n_obs_per_arm, arm_idxs, self.n_arms
-            )
-            sum_reward_per_arm = _place_values_at_indices(
-                sum_reward_per_arm, arm_idxs, self.n_arms
-            )
-
-        self.total_n_obs_per_arm += n_obs_per_arm
-        self.total_sum_reward_per_arm += sum_reward_per_arm
-        self.total_n_obs_all_arms += int(n_obs_per_arm.sum())
-
-    def add_single_observation(self, arm_id: int, reward: float):
-        assert arm_id in self.arm_ids
-        arm_idx = self.arm_ids.index(arm_id)
-        self.total_n_obs_per_arm[arm_idx] += 1
-        self.total_sum_reward_per_arm[arm_idx] += reward
-        self.total_n_obs_all_arms += 1
-
-    def get_avg_reward_values(self) -> Tensor:
-        return self.total_sum_reward_per_arm / self.total_n_obs_per_arm
-
-    def get_action(self) -> Union[str, int]:
-        """
-        Get the id of the action chosen by the UCB algorithm
-
-        Returns:
-            int: The integer ID of the chosen action
-        """
-        ucb_scores = self.get_ucb_scores()
-        return self.arm_ids[torch.argmax(ucb_scores)]
-
-    @classmethod
-    def get_ucb_scores_from_batch(
-        cls,
-        n_obs_per_arm: Tensor,
-        sum_reward_per_arm: Tensor,
-        *args,
-        **kwargs,
-    ) -> Tensor:
-        """
-        A utility method used to create the bandit, feed in a batch of observations and get the UCB scores in one function call
-
-        Args:
-            n_obs_per_arm (Tensor): An array of counts of per-arm numbers of observations
-            sum_reward_per_arm (Tensor): An array of sums of rewards for each arm
-            (additional arguments can be provided for specific concrete class implementations)
-
-        Returns:
-            Tensor: Array of per-arm UCB scores
-        """
-        n_arms = len(n_obs_per_arm)
-        b = cls(n_arms=n_arms)
-        b.add_batch_observations(n_obs_per_arm, sum_reward_per_arm, *args, **kwargs)
-        return b.get_ucb_scores()
 
     @abstractmethod
     def get_ucb_scores(self):
@@ -181,74 +59,6 @@ class UCBTuned(BaseUCB):
     Biggest difference from basic UCB is that per-arm reward variance is estimated.
     """
 
-    # _fields_for_saving = BaseUCB._fields_for_saving + [
-    #     "total_sum_reward_squared_per_arm"
-    # ]
-
-    def __init__(
-        self,
-        n_arms: Optional[int] = None,
-        arm_ids: Optional[List[Union[str, int]]] = None,
-    ):
-        super(UCBTuned, self).__init__(n_arms=n_arms, arm_ids=arm_ids)
-        self.total_sum_reward_squared_per_arm = torch.zeros(self.n_arms)
-
-    def add_batch_observations(
-        self,
-        n_obs_per_arm: Tensor,
-        sum_reward_per_arm: Tensor,
-        sum_reward_squared_per_arm: Tensor,
-        arm_ids: Optional[List[Union[str, int]]] = None,
-    ):
-        """
-        Add information about arm rewards in a batched form.
-
-        Args:
-            n_obs_per_arm (Tensor): An array of counts of per-arm numbers of observations
-            sum_reward_per_arm (Tensor): An array of sums of rewards for each arm
-            sum_reward_squared_per_arm (Tensor): An array of sums of squares of rewards for each arm
-            arm_ids (Optional[List[Union[str, int]]]): A list of ids of arms in the same order as the elements of previous arrays
-        """
-        assert len(sum_reward_per_arm) == len(sum_reward_squared_per_arm)
-        super().add_batch_observations(
-            n_obs_per_arm, sum_reward_per_arm, arm_ids=arm_ids
-        )
-        if not isinstance(sum_reward_per_arm, Tensor):
-            sum_reward_squared_per_arm = torch.tensor(
-                sum_reward_squared_per_arm, dtype=torch.float
-            )
-
-        if arm_ids is None or arm_ids == self.arm_ids:
-            # assume that the observations are for all arms in the default order
-            arm_ids = self.arm_ids
-            arm_idxs = list(range(self.n_arms))
-        else:
-            assert len(arm_ids) == len(
-                set(arm_ids)
-            )  # make sure no duplicates in arm IDs
-
-            # get the indices of the arms
-            arm_idxs = _get_arm_indices(self.arm_ids, arm_ids)
-
-            # put elements from the batch in the positions specified by `arm_ids` (missing arms will be zero)
-            sum_reward_squared_per_arm = _place_values_at_indices(
-                sum_reward_squared_per_arm, arm_idxs, self.n_arms
-            )
-
-        self.total_sum_reward_squared_per_arm += sum_reward_squared_per_arm
-
-    def add_single_observation(self, arm_id: int, reward: float):
-        """
-        Add a single observation (arm played, reward) to the bandit
-
-        Args:
-            arm_id (int): Which arm was played
-            reward (float): Reward renerated by the arm
-        """
-        super().add_single_observation(arm_id, reward)
-        arm_idx = self.arm_ids.index(arm_id)
-        self.total_sum_reward_squared_per_arm[arm_idx] += reward ** 2
-
     def get_ucb_scores(self) -> Tensor:
         """
         Get per-arm UCB scores. The formula is
@@ -276,26 +86,6 @@ class UCBTuned(BaseUCB):
             self.total_n_obs_per_arm > 0,
             ucb,
             torch.tensor(torch.inf, dtype=torch.float),
-        )
-
-
-class UCBTunedBernoulli(UCBTuned):
-    def add_batch_observations(
-        self,
-        n_obs_per_arm: Tensor,
-        num_success_per_arm: Tensor,
-        arm_ids: Optional[List[Union[str, int]]] = None,
-    ):
-        """
-        Add a batch of observations to the UCBTuned bandit, assuming Bernoulli distribution of rewards.
-        Because of the Bernoulli assumption, we don't need to provide the squared rewards separately
-
-        Args:
-            n_obs_per_arm (Tensor): An array of counts of per-arm numbers of observations
-            num_success_per_arm (Tensor): An array of counts of per-arm numbers of successes
-        """
-        super().add_batch_observations(
-            n_obs_per_arm, num_success_per_arm, num_success_per_arm, arm_ids=arm_ids
         )
 
 
