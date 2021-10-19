8f29370e11a42f54edc5330764bd04361f0932ae
Alex Nikulkov <alexnik@fb.com>
2021-10-18 17:41:24 -0700

    Add variance estimates to UCB
    Summary:
    1. Add option to estimate reward variance and scale the confidence interval width by SQRT(VAR).
    2. Add an option to multiply confidence interval width by a constant scalar to make exploration more/less aggressive
    3. Remove UCBTuned algorithm because it is essentially UCB1 + variance estimation
    
    Reviewed By: czxttkl
    
    Differential Revision: D31741828
    
    fbshipit-source-id: 684788746e2e626228cb522c49b2bafa9179d6fe

--- reagent/mab/ucb.py
+++ reagent/mab/ucb.py
@@ -1,5 +1,6 @@
 import math
 from abc import ABC, abstractmethod
+from typing import Optional, List
 
 import torch
 from reagent.mab.mab_algorithm import MABAlgo
@@ -9,14 +10,41 @@ from torch import Tensor
 class BaseUCB(MABAlgo, ABC):
     """
     Base class for UCB-like Multi-Armed Bandits (MAB)
+
+    Args:
+        estimate_variance: If True, per-arm reward variance is estimated and we multiply thconfidence interval width
+            by its square root
+        alpha: Scalar multiplier for confidence interval width. Values above 1.0 make exploration more aggressive, below 1.0 less aggressive
     """
 
+    def __init__(
+        self,
+        estimate_variance: bool = True,
+        alpha: float = 1.0,
+        *,
+        n_arms: Optional[int] = None,
+        arm_ids: Optional[List[str]] = None,
+    ):
+        super().__init__(n_arms=n_arms, arm_ids=arm_ids)
+        self.estimate_variance = estimate_variance
+        self.alpha = alpha
+
     @abstractmethod
-    def get_ucb_scores(self):
+    def get_ucb_scores(self) -> Tensor:
         pass
 
-    def forward(self):
+    def forward(self) -> Tensor:
         return self.get_ucb_scores()
+
+    @property
+    def var(self):
+        # return empirical variance of rewards for each arm
+        if self.estimate_variance:
+            return self.total_sum_reward_squared_per_arm / self.total_n_obs_per_arm - (
+                (self.total_sum_reward_per_arm / self.total_n_obs_per_arm) ** 2
+            )
+        else:
+            return 1.0
 
 
 class UCB1(BaseUCB):
@@ -25,39 +53,11 @@ class UCB1(BaseUCB):
     Reference: https://www.cs.bham.ac.uk/internal/courses/robotics/lectures/ucb1.pdf
     """
 
-    def get_ucb_scores(self):
-        """
-        Get per-arm UCB scores. The formula is
-        UCB_i = AVG([rewards_i]) + SQRT(2*LN(T)/N_i)
-
-        Returns:
-            Tensor: An array of UCB scores (one per arm)
-        """
-        avg_rewards = self.get_avg_reward_values()
-        log_t_over_ni = (
-            math.log(self.total_n_obs_all_arms + 1) / self.total_n_obs_per_arm
-        )
-        ucb = avg_rewards + torch.sqrt(2 * log_t_over_ni)
-        return torch.where(
-            self.total_n_obs_per_arm > 0,
-            ucb,
-            torch.tensor(torch.inf, dtype=torch.float),
-        )
-
-
-class UCBTuned(BaseUCB):
-    """
-    Implementation of the UCB-Tuned algorithm from Section 4 of  https://link.springer.com/content/pdf/10.1023/A:1013689704352.pdf
-    Biggest difference from basic UCB is that per-arm reward variance is estimated.
-    """
-
     def get_ucb_scores(self) -> Tensor:
         """
         Get per-arm UCB scores. The formula is
-        UCB_i = AVG([rewards_i]) + SQRT(LN(T)/N_i * V_i)
-        where V_i is a conservative variance estimate of arm i:
-            V_i = AVG([rewards_i**2]) - AVG([rewards_i])**2 + sqrt(2ln(t) / n_i)
-        Nore that we don't apply the min(1/4, ...) operator to the variance because this bandit is meant for non-Bernoulli applications as well
+        UCB_i = AVG([rewards_i]) + SQRT(2*LN(T)/N_i*VAR)
+        VAR=1 if estimate_variance==False, otherwise VAR=AVG([rewards_i**2]) - AVG([rewards_i])**2
 
         Returns:
             Tensor: An array of UCB scores (one per arm)
@@ -66,14 +66,7 @@ class UCBTuned(BaseUCB):
         log_t_over_ni = (
             math.log(self.total_n_obs_all_arms + 1) / self.total_n_obs_per_arm
         )
-        per_arm_var_est = (
-            self.total_sum_reward_squared_per_arm / self.total_n_obs_per_arm
-            - avg_rewards ** 2
-            + torch.sqrt(
-                2 * log_t_over_ni
-            )  # additional term to make the estimate conservative (unlikely to underestimate)
-        )
-        ucb = avg_rewards + torch.sqrt(log_t_over_ni * per_arm_var_est)
+        ucb = avg_rewards + self.alpha * torch.sqrt(2 * log_t_over_ni * self.var)
         return torch.where(
             self.total_n_obs_per_arm > 0,
             ucb,
@@ -84,10 +77,11 @@ class UCBTuned(BaseUCB):
 class MetricUCB(BaseUCB):
     """
     This is an improvement over UCB1 which uses a more precise confidence radius, especially for small expected rewards.
+    This algorithm has been constructed for Benroulli reward distributions.
     Reference: https://arxiv.org/pdf/0809.4882.pdf
     """
 
-    def get_ucb_scores(self):
+    def get_ucb_scores(self) -> Tensor:
         """
         Get per-arm UCB scores. The formula is
         UCB_i = AVG([rewards_i]) + SQRT(AVG([rewards_i]) * LN(T+1)/N_i) + LN(T+1)/N_i
@@ -99,7 +93,9 @@ class MetricUCB(BaseUCB):
         log_t_over_ni = (
             math.log(self.total_n_obs_all_arms + 1) / self.total_n_obs_per_arm
         )
-        ucb = avg_rewards + torch.sqrt(avg_rewards * log_t_over_ni) + log_t_over_ni
+        ucb = avg_rewards + self.alpha * (
+            torch.sqrt(avg_rewards * log_t_over_ni) + log_t_over_ni
+        )
         return torch.where(
             self.total_n_obs_per_arm > 0,
             ucb,
