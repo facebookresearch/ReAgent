830317942f045bb72646c414c6e708796d7ea468
Geng Ji <gji@fb.com>
2021-12-04 17:02:56 -0800

    Add OSS BanditRewardNetPredictorWrapper and enable exporting reward models for cfeval (#590)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/590
    
    Added an OSS version of FbBanditRewardNetPredictorWrapper.
    
    Enabled reward-model serving modules for OSS/internal DQN/CRR model managers.
    
    Reviewed By: czxttkl
    
    Differential Revision: D32818767
    
    fbshipit-source-id: eb9d30295c0778fce3daa6d2d686917c11f2cc4a

---
+++ reagent/prediction/cfeval/predictor_wrapper.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
+import logging
+from typing import List, Tuple
+
+import torch
+from reagent.core import types as rlt
+from reagent.prediction.predictor_wrapper import DiscreteDqnWithPreprocessor
+
+logger = logging.getLogger(__name__)
+
+
+class BanditRewardNetPredictorWrapper(torch.jit.ScriptModule):
+    def __init__(
+        self,
+        reward_model_with_preprocessor: DiscreteDqnWithPreprocessor,
+        action_names: List[str],
+        state_feature_config: rlt.ModelFeatureConfig,
+    ) -> None:
+        super().__init__()
+        self.reward_model_with_preprocessor = torch.jit.trace(
+            reward_model_with_preprocessor,
+            reward_model_with_preprocessor.input_prototype(),
+        )
+        self.action_names = torch.jit.Attribute(action_names, List[str])
+
+    @torch.jit.script_method
+    def forward(
+        self, state: rlt.ServingFeatureData
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        reward_predictions = self.reward_model_with_preprocessor(state)
+        num_examples = reward_predictions.size()[0]
+        num_actions = len(self.action_names)
+        assert reward_predictions.shape == (
+            num_examples,
+            num_actions,
+        ), f"Invalid shape {reward_predictions.shape} != ({num_examples}, {num_actions})"
+        mask = torch.ones_like(reward_predictions, dtype=torch.uint8)
+        return (reward_predictions, mask)
