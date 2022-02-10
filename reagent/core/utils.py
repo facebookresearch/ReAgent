9d51f0125e9378bd353624f83304bddf77449045
Zhengxing Chen <czxttkl@fb.com>
2022-02-09 16:56:07 -0800

    Add net builder of sparse arch-based reward decomposition models
    Summary: As a showcase for how to add sparse features to ReAgent
    
    Differential Revision: D34082047
    
    fbshipit-source-id: e0a8ec6f636c531b6fa4b3d0ac340c77ef2fb782

--- reagent/core/utils.py
+++ reagent/core/utils.py
@@ -2,7 +2,58 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
 
 
+import logging
+from collections import defaultdict
+from typing import List, Dict
+
+import reagent.core.types as rlt
 import torch
+from torchrec import EmbeddingBagConfig
+
+logger = logging.getLogger(__name__)
+
+
+def embedding_bag_configs_from_feature_configs(
+    configs: List[rlt.ModelFeatureConfig],
+) -> List[EmbeddingBagConfig]:
+    """
+    Obtain a list of EmbeddingBagConfigs from multiple ModelFeatureConfigs.
+    The returned list will be used for defining sparse model architectures
+    """
+    merged_id_mapping_config: Dict[str, rlt.IdMappingConfig] = {}
+    for config in configs:
+        for id_mapping_name, id_mapping_config in config.id_mapping_config.items():
+            if id_mapping_name in merged_id_mapping_config:
+                assert (
+                    merged_id_mapping_config[id_mapping_name] == id_mapping_config
+                ), f"Conflicting IdMappingConfigs for id_mapping_name={id_mapping_name}"
+            else:
+                merged_id_mapping_config[id_mapping_name] = id_mapping_config
+
+    id_mapping_to_feature_names = defaultdict(list)
+    for config in configs:
+        for id_list_feature_config in config.id_list_feature_configs:
+            id_mapping_to_feature_names[id_list_feature_config.id_mapping_name].append(
+                id_list_feature_config.name
+            )
+        for id_score_list_feature_config in config.id_score_list_feature_configs:
+            id_mapping_to_feature_names[
+                id_score_list_feature_config.id_mapping_name
+            ].append(id_score_list_feature_config.name)
+
+    embedding_bag_configs: List[EmbeddingBagConfig] = []
+    for id_mapping_name, config in merged_id_mapping_config.items():
+        embedding_bag_configs.append(
+            EmbeddingBagConfig(
+                name=id_mapping_name,
+                feature_names=id_mapping_to_feature_names[id_mapping_name],
+                num_embeddings=config.embedding_table_size,
+                embedding_dim=config.embedding_dim,
+                pooling=config.pooling_type,
+            )
+        )
+    logger.info(f"Generate EmbeddingBagConfigs: {embedding_bag_configs}")
+    return embedding_bag_configs
 
 
 def get_rank() -> int:
--- reagent/net_builder/synthetic_reward/transformer_synthetic_reward.py
+++ reagent/net_builder/synthetic_reward/transformer_synthetic_reward.py
@@ -3,6 +3,7 @@
 
 from typing import List, Optional
 
+import reagent.core.types as rlt
 from reagent.core.dataclasses import dataclass
 from reagent.core.parameters import NormalizationData, param_hash
 from reagent.models.base import ModelBase
@@ -33,6 +34,8 @@ class TransformerSyntheticReward(SyntheticRewardNetBuilder):
         state_normalization_data: NormalizationData,
         action_normalization_data: Optional[NormalizationData] = None,
         discrete_action_names: Optional[List[str]] = None,
+        state_feature_config: Optional[rlt.ModelFeatureConfig] = None,
+        action_feature_config: Optional[rlt.ModelFeatureConfig] = None,
     ) -> ModelBase:
         state_dim = get_num_output_features(
             state_normalization_data.dense_normalization_parameters
