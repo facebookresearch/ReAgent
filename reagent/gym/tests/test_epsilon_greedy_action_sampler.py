066398cdd576e31027853e174c0f5f2e365e8478
Quintin Fettes <qfettes@fb.com>
2022-04-21 12:09:52 -0700

    Fix EpsilonGreedyActionSampler Runtime error and add a test to ensure greedy action selection is working (#632)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/632
    
    This diff fixes a runtime error with the EpsilonGreedyActionSampler, and adds a unit test to ensure that greedy action selection via this class computes correct log probs and returns the expected actions.
    
    Reviewed By: czxttkl
    
    Differential Revision: D35783785
    
    fbshipit-source-id: e6d64ea0dbd643e3887ed47497f37c005c518276

---
+++ reagent/gym/tests/test_epsilon_greedy_action_sampler.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+import torch
+from reagent.gym.policies.samplers.discrete_sampler import EpsilonGreedyActionSampler
+from reagent.test.base.horizon_test_base import HorizonTestBase
+
+
+class EpsilonGreedyActionSamplerTest(HorizonTestBase):
+    def test_greedy_selection(self):
+        scores = torch.tensor(
+            [
+                [1.0, 2.0, 3.0, 4.0, 5.0],
+                [5.0, 1.0, 2.0, 3.0, 4.0],
+            ]
+        )
+        sampler = EpsilonGreedyActionSampler(epsilon=0.0)
+
+        test_action = torch.tensor(
+            [
+                [0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0],
+            ],
+            dtype=torch.long,
+        )
+        action = sampler.sample_action(scores)
+
+        torch.testing.assert_allclose(action.action, test_action)
+
+        test_log_prob = torch.tensor(
+            [0.0, 0.0],
+            dtype=torch.float,
+        )
+
+        torch.testing.assert_allclose(action.log_prob, test_log_prob)
+
+    def test_uniform_random_selection(self):
+        scores = torch.tensor(
+            [
+                [1.0, 2.0, 3.0, 4.0, 5.0],
+                [5.0, 1.0, 2.0, 3.0, 4.0],
+            ]
+        )
+        sampler = EpsilonGreedyActionSampler(epsilon=1.0)
+
+        action = sampler.sample_action(scores)
+
+        test_log_prob = torch.tensor(
+            [-1.60944, -1.60944],
+            dtype=torch.float,
+        )
+
+        torch.testing.assert_allclose(action.log_prob, test_log_prob)
