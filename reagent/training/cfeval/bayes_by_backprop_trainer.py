607df971f74fd21a0612353e11839e37a651393a
Ronald Yu <ronaldyu@fb.com>
2022-05-06 16:10:42 -0700

    Bayes by Backprop (#637)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/637
    
    Simple implementation of the Bayesian Network tutorial at https://www.internalfb.com/intern/anp/view/?id=1798614 in the context of an reagent model and trainer.
    
    Reviewed By: czxttkl
    
    Differential Revision: D35852022
    
    fbshipit-source-id: c91b8982129b747bdf2049ba38a8b7c447002548

---
+++ reagent/training/cfeval/bayes_by_backprop_trainer.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
+import logging
+
+import reagent.core.types as rlt
+import torch
+from reagent.training.cfeval.bandit_reward_network_trainer import BanditRewardNetTrainer
+
+logger = logging.getLogger(__name__)
+
+
+class BayesByBackpropTrainer(BanditRewardNetTrainer):
+    def train_step_gen(
+        self, training_batch: rlt.BanditRewardModelInput, batch_idx: int
+    ):
+        weight = self._get_sample_weight(training_batch)
+
+        # pyre-ignore seems to be pyre bug for pytorch
+        loss = self.reward_net.sample_elbo(
+            torch.cat([training_batch.action, training_batch.state.float_features], 1),
+            training_batch.reward,
+            1,
+        )
+        # use (action, reward) to indicate (input,target)
+
+        detached_loss = loss.detach().cpu()
+        self.reporter.log(loss=detached_loss)
+
+        if weight is not None:
+            raise NotImplementedError  # TODO for integration in to RL framework
+            # unweighted_loss = self._compute_unweighted_loss(
+            #    predicted_reward, target_reward, training_batch
+            # )
+            # self.reporter.log(unweighted_loss=unweighted_loss)
+
+        self.all_batches_processed += 1
+        if self.all_batches_processed % 100 == 0:
+            logger.info(
+                f"{self.all_batches_processed}-th batch: "
+                f"Loss={detached_loss.item()}"
+            )
+
+        yield loss
+
+    def validation_step(self, batch: rlt.BanditRewardModelInput, batch_idx: int):
+        if self._training_batch_type and isinstance(batch, dict):
+            batch = self._training_batch_type.from_dict(batch)
+
+        weight = self._get_sample_weight(batch)
+        # pyre-ignore
+        loss = self.reward_net.sample_elbo(
+            torch.cat([batch.action, batch.state.float_features], 1),
+            batch.reward,
+            1,
+        )
+
+        detached_loss = loss.detach().cpu()
+        self.reporter.log(eval_loss=detached_loss)
+
+        if weight is not None:
+            raise NotImplementedError  # TODO for integration in to RL framework
+            # unweighted_loss = self._compute_unweighted_loss(pred_reward, reward, batch)
+            # self.reporter.log(eval_unweighted_loss=unweighted_loss)
+
+        return detached_loss.item()
