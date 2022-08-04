de7d782398bcd10da744aee81d8549e6daff1e0d
Urun Dogan <urundogan@fb.com>
2022-08-03 20:52:38 -0700

    Sparse DQN Implementation (#663)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/663
    
    This is the first part of the diff. I have moved the model in to reagent. Moreover, I have made some refactoring.
    
    Reviewed By: czxttkl
    
    Differential Revision: D38011818
    
    fbshipit-source-id: cb76646ed0e3149887180cbe642b1035afaace9b

---
+++ reagent/models/sparse_dqn.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
+from typing import List
+
+import torch
+from reagent.core import types as rlt
+from reagent.models import FullyConnectedNetwork
+from reagent.models.base import ModelBase
+from torchrec.models.dlrm import SparseArch
+from torchrec.modules.embedding_modules import EmbeddingBagCollection
+from torchrec.sparse.jagged_tensor import KeyedJaggedTensor
+
+
+class SparseDQN(ModelBase):
+    """
+    Concatenating embeddings from bag collection with float features before passing the input
+    to DQN
+    """
+
+    def __init__(
+        self,
+        state_dense_dim: int,
+        embedding_bag_collection: EmbeddingBagCollection,
+        action_dense_dim: int,
+        overarch_dims: List[int],
+        activation: str = "relu",
+        final_activation: str = "relu",
+        use_batch_norm: bool = True,
+        use_layer_norm: bool = False,
+        output_dim: int = 1,
+    ) -> None:
+        super().__init__()
+        self.sparse_arch: SparseArch = SparseArch(embedding_bag_collection)
+
+        self.sparse_embedding_dim: int = sum(
+            [
+                len(embc.feature_names) * embc.embedding_dim
+                for embc in embedding_bag_collection.embedding_bag_configs()
+            ]
+        )
+
+        self.input_dim: int = (
+            state_dense_dim + self.sparse_embedding_dim + action_dense_dim
+        )
+        layers = [self.input_dim] + overarch_dims + [output_dim]
+        activations = [activation] * len(overarch_dims) + [final_activation]
+        self.q_network = FullyConnectedNetwork(
+            layers=layers,
+            activations=activations,
+            use_batch_norm=use_batch_norm,
+        )
+
+    def fetch_id_list_features(
+        self, state: rlt.FeatureData, action: rlt.FeatureData
+    ) -> KeyedJaggedTensor:
+        assert state.id_list_features is not None or action.id_list_features is not None
+        if state.id_list_features is not None and action.id_list_features is None:
+            sparse_features = state.id_list_features
+        elif state.id_list_features is None and action.id_list_features is not None:
+            sparse_features = action.id_list_features
+        elif state.id_list_features is not None and action.id_list_features is not None:
+            sparse_features = KeyedJaggedTensor.concat(
+                [state.id_list_features, action.id_list_features]
+            )
+        else:
+            raise ValueError
+        # TODO: add id_list_score_features
+        return sparse_features
+
+    def forward(self, state: rlt.FeatureData, action: rlt.FeatureData) -> torch.Tensor:
+        dense_features = torch.cat(
+            (state.float_features, action.float_features), dim=-1
+        )
+        sparse_features = self.fetch_id_list_features(state, action)
+        embedded_sparse = self.sparse_arch(sparse_features)
+        concatenated_dense = torch.cat((dense_features, embedded_sparse), dim=-1)
+
+        return self.q_network(concatenated_dense)
---
+++ reagent/test/models/test_sparse_dqn_net.py
@@ -0,0 +1,52 @@
+import unittest
+
+import torch
+from reagent.models.sparse_dqn import SparseDQN
+from torchrec import EmbeddingBagCollection, EmbeddingBagConfig
+
+
+class TestSparseDQN(unittest.TestCase):
+    def test_single_step_sparse_dqn(self):
+        state_dense_dim = 10
+        action_dense_dim = 2
+        dense_sizes = [256, 32]
+        activation = "relu"
+        final_activation = "relu"
+        # Fake embedding bag configs
+        embedding_table_size = 1000
+        embedding_dim = 32
+        num_sparse_features = 2  # refer to watched_ids and liked_ids below
+        embedding_bag_configs = [
+            EmbeddingBagConfig(
+                name="video_id",
+                feature_names=["watched_ids", "liked_ids"],
+                num_embeddings=embedding_table_size,
+                embedding_dim=embedding_dim,
+            )
+        ]
+        embedding_bag_col = EmbeddingBagCollection(
+            device=torch.device("cpu"), tables=embedding_bag_configs
+        )
+
+        net = SparseDQN(
+            state_dense_dim=state_dense_dim,
+            embedding_bag_collection=embedding_bag_col,
+            action_dense_dim=action_dense_dim,
+            overarch_dims=dense_sizes,
+            activation=activation,
+            final_activation=final_activation,
+            output_dim=action_dense_dim,
+        ).q_network.dnn
+
+        # the dim of the input to overall arch is dimension of dense features plus
+        # number of sparse features times embedding dimension for sparse features
+        assert (
+            net[1].in_features
+            == state_dense_dim + action_dense_dim + num_sparse_features * embedding_dim
+        )
+        assert net[1].out_features == dense_sizes[0]
+        assert net[4].in_features == dense_sizes[0]
+        assert net[4].out_features == dense_sizes[1]
+        assert net[7].in_features == dense_sizes[1]
+
+        assert net[7].out_features == action_dense_dim
