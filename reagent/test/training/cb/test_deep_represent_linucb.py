3203ee8f6cf1241a640257f792865ccd98d17494
Hongbo Guo <hongbog@fb.com>
2022-08-15 22:48:42 -0700

    Reagent DeepRepresentLinucb [1/x] (#664)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/664
    
    Implement Reagent DeepRepresentLinUCB.
    
    It is a multiple layer regression model that output UCB score.
    The first N layers are trainable by torch optimizer().
    The last layer is the traditional LinUCB, and it is not updated by optimizer, but still will be updated by matrix computations.
    
    {F760388037}
    
    Reviewed By: alexnikulkov
    
    Differential Revision: D38268001
    
    fbshipit-source-id: d739e6af0cfecd891681d1e736d3191547441c92

---
+++ reagent/test/training/cb/test_deep_represent_linucb.py
@@ -0,0 +1,75 @@
+import unittest
+
+import numpy as np
+import numpy.testing as npt
+import torch
+from reagent.core.types import CBInput
+from reagent.gym.policies.policy import Policy
+from reagent.gym.policies.samplers.discrete_sampler import GreedyActionSampler
+
+from reagent.models.deep_represent_linucb import DeepRepresentLinearRegressionUCB
+from reagent.models.fully_connected_network import FullyConnectedNetwork
+from reagent.training.cb.deep_represent_linucb_trainer import DeepRepresentLinUCBTrainer
+from reagent.training.parameters import LinUCBTrainerParameters
+
+
+class TestDeepRepresentLinUCB(unittest.TestCase):
+    def setUp(self):
+
+        self.params = LinUCBTrainerParameters()
+
+        raw_input_dim = 100
+        sizes = [100]
+        linucb_inp_dim = 100
+        activations = ["relu"]
+        output_activation = "linear"
+
+        customized_layers = FullyConnectedNetwork(
+            [raw_input_dim] + sizes + [linucb_inp_dim],
+            activations + [output_activation],
+            use_batch_norm=False,
+            dropout_ratio=0.0,
+            normalize_output=False,
+            use_layer_norm=False,
+        )
+        policy_network = DeepRepresentLinearRegressionUCB(
+            raw_input_dim=raw_input_dim,
+            sizes=sizes,
+            linucb_inp_dim=linucb_inp_dim,
+            activations=activations,
+            mlp_layers=customized_layers,
+        )
+
+        self.policy = Policy(scorer=policy_network, sampler=GreedyActionSampler())
+        self.trainer = DeepRepresentLinUCBTrainer(self.policy, **self.params.asdict())
+        self.batch = CBInput(
+            context_arm_features=torch.rand(2, 2, 100),
+            action=torch.tensor([[0], [1]], dtype=torch.long),
+            reward=torch.tensor([[1.5], [-2.3]]),
+        )  # random Gaussian features where feature_dim=100
+
+    def test_linucb_training_step(self):
+        self.trainer.training_step(self.batch, 0)
+        assert len(self.batch.action) == len(self.batch.reward)
+        assert len(self.batch.action) == self.batch.context_arm_features.shape[0]
+
+        loss_iterations = []
+        for _ in range(100):
+            # Linucb parameters are updated within training_step manually
+            loss = self.trainer.training_step(batch=self.batch, batch_idx=0).item()
+            loss_iterations.append(loss)
+
+        npt.assert_allclose(
+            np.asarray(loss_iterations[90:]),
+            np.zeros(10),
+            atol=1e-2,
+        )
+        return loss_iterations
+
+
+"""
+How to use:
+    test = TestDeepRepresentLinUCB()
+    test.setUp()
+    losses = test.test_linucb_training_step()
+"""
