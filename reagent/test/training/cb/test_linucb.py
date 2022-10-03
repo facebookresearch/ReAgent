577d7eab59d4958d994defdd4226846b8360b57b
Alex Nikulkov <alexnik@meta.com>
2022-10-03 10:29:12 -0700

    add discounting to LinUCB (#685)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/ReAgent/pull/685
    
    Mostly a copy of D39157916 for Joint LinUCB model
    
    Differential Revision: D39929407
    
    fbshipit-source-id: 7566c00641f1b467c4d92a25d213ac7b0c9b4bbd

--- reagent/test/training/cb/test_linucb.py
+++ reagent/test/training/cb/test_linucb.py
@@ -30,13 +30,11 @@ class TestLinUCButils(unittest.TestCase):
 class TestLinUCB(unittest.TestCase):
     def setUp(self):
         self.batch_size = 2
-        self.state_dim = 2
-        self.arm_dim = 2
 
         self.num_arms = 2
         self.params = LinUCBTrainerParameters()
 
-        self.x_dim = 1 + self.state_dim * self.num_arms + self.state_dim + self.num_arms
+        self.x_dim = 5
         policy_network = LinearRegressionUCB(self.x_dim)
         self.policy = Policy(scorer=policy_network, sampler=GreedyActionSampler())
 
@@ -45,12 +43,12 @@ class TestLinUCB(unittest.TestCase):
             context_arm_features=torch.tensor(
                 [
                     [
-                        [1, 2, 3, 6, 7, 2 * 6, 2 * 7, 3 * 6, 3 * 7],
-                        [1, 2, 3, 10, 11, 2 * 10, 2 * 11, 3 * 10, 3 * 11],
+                        [1, 2, 3, 6, 7],
+                        [1, 2, 3, 10, 11],
                     ],
                     [
-                        [1, 4, 5, 8, 9, 4 * 8, 4 * 9, 5 * 8, 5 * 9],
-                        [1, 4, 5, 12, 13, 4 * 12, 4 * 13, 5 * 12, 5 * 13],
+                        [1, 4, 5, 8, 9],
+                        [1, 4, 5, 12, 13],
                     ],
                 ],
                 dtype=torch.float,
@@ -184,3 +182,85 @@ class TestLinUCB(unittest.TestCase):
         )  # make sure A got updated
         npt.assert_allclose(scorer_1.A.numpy(), scorer_2.A.numpy(), rtol=1e-4)
         npt.assert_allclose(scorer_1.b.numpy(), scorer_2.b.numpy(), rtol=1e-4)
+
+    def test_linucb_discount_factors(self) -> None:
+        # change the precision to double
+        torch.set_default_dtype(torch.float64)
+
+        gamma = 0.9
+        scorer = LinearRegressionUCB(self.x_dim, gamma=gamma)
+        policy = Policy(scorer=scorer, sampler=GreedyActionSampler())
+        trainer = LinUCBTrainer(policy)
+
+        # 1st round training
+        trainer.training_step(self.batch, 0)
+        trainer.on_train_epoch_end()
+
+        # 2nd round training
+        torch.manual_seed(0)
+        self.batch_2nd_round = CBInput(
+            context_arm_features=torch.randn((10, self.num_arms, self.x_dim)),
+            reward=torch.randn((10, 1)),
+            action=torch.tensor([[0], [1]], dtype=torch.long).repeat(5, 1),
+        )
+        self.second_batch_2nd_round = CBInput(
+            context_arm_features=torch.randn((6, self.num_arms, self.x_dim)),
+            reward=torch.randn((6, 1)),
+            action=torch.tensor([[0], [1]], dtype=torch.long).repeat(3, 1),
+        )
+        trainer.training_step(self.batch_2nd_round, 0)
+        # check if there are several training steps in a round
+        # discount factor is only applied one time
+        trainer.training_step(self.second_batch_2nd_round, 1)
+        trainer.on_train_epoch_end()
+
+        # eval dataset
+        inp1 = torch.randn((5, self.x_dim))
+        out1 = scorer(inp1)
+        # check it won't do redundant coefficent update after 2nd eval
+        inp2 = torch.randn((5, self.x_dim))
+        out2 = scorer(inp2)
+
+        # the feature matrix and model parameter and eval output (computed by hand)
+        x1 = _get_chosen_arm_features(
+            self.batch.context_arm_features, self.batch.action
+        ).numpy()
+        x2 = _get_chosen_arm_features(
+            self.batch_2nd_round.context_arm_features, self.batch_2nd_round.action
+        ).numpy()
+        x3 = _get_chosen_arm_features(
+            self.second_batch_2nd_round.context_arm_features,
+            self.second_batch_2nd_round.action,
+        ).numpy()
+        reward1 = self.batch.reward.squeeze().numpy()
+        reward2 = self.batch_2nd_round.reward.squeeze().numpy()
+        reward3 = self.second_batch_2nd_round.reward.squeeze().numpy()
+
+        # all matrix and vectors are the same
+        A = scorer.gamma * x1.T @ x1 + x2.T @ x2 + x3.T @ x3
+        b = scorer.gamma * x1.T @ reward1 + x2.T @ reward2 + x3.T @ reward3
+        npt.assert_allclose(scorer.A.numpy(), scorer.gamma * A, atol=1e-5, rtol=1e-5)
+        npt.assert_allclose(scorer.b.numpy(), scorer.gamma * b, atol=1e-5, rtol=1e-5)
+
+        inv_A = np.linalg.inv(A + np.identity(self.x_dim) * scorer.l2_reg_lambda)
+        npt.assert_allclose(scorer.inv_A.numpy(), inv_A, atol=1e-4, rtol=1e-4)
+
+        # model parameters are the same
+        theta = inv_A @ b
+        npt.assert_allclose(scorer.coefs.numpy(), theta, atol=1e-4, rtol=1e-4)
+
+        # ucb scores are the same
+        def calculated_expected_ucb_scores(inp):
+            expected_out = np.zeros(inp.size()[0])
+            for i in range(inp.size()[0]):
+                x = inp[i].numpy()
+                expected_out[i] = x @ theta.T + scorer.ucb_alpha * np.sqrt(
+                    x @ inv_A @ x.T
+                )
+            return expected_out
+
+        expected_out1 = calculated_expected_ucb_scores(inp1)
+        npt.assert_allclose(out1.numpy(), expected_out1, atol=1e-4, rtol=1e-4)
+
+        expected_out2 = calculated_expected_ucb_scores(inp2)
+        npt.assert_allclose(out2.numpy(), expected_out2, atol=1e-4, rtol=1e-4)
