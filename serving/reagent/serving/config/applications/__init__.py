ae94bca42f1bf98072b79cbeb36720e600d62c0a
Jason Fried <fried@meta.com>
2025-03-11 20:53:03 -0700

    fix deprecated find_module().load_module()
    Summary: First pass at fixing this copy pasta from stackoverflow.   I'll cover more usages of walk_packages in my next diff
    
    Reviewed By: itamaro
    
    Differential Revision: D70987465
    
    fbshipit-source-id: f9a19be8b2bae322bc757b024077ef1207d82b3c

--- serving/reagent/serving/config/applications/__init__.py
+++ serving/reagent/serving/config/applications/__init__.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
-# pyre-unsafe
-
+import importlib
 import pkgutil
 
 
 __all__ = []
 print(list(pkgutil.walk_packages(__path__)))
 
-for loader, module_name, _ in pkgutil.walk_packages(__path__):
+for _, module_name, _ in pkgutil.walk_packages(__path__):
     __all__.append(module_name)
-    # pyre-fixme[16]: Item `MetaPathFinderProtocol` of `MetaPathFinderProtocol |
-    #  PathEntryFinderProtocol` has no attribute `find_module`.
-    _module = loader.find_module(module_name).load_module(module_name)
+    _module = importlib.import_module(f"{__name__}.{module_name}")
     globals()[module_name] = _module
