dde83eae9154945d3cff94cd83ff89abeff36655
generatedunixname1228101065403401 <noreply+1228101065403401@fb.com>
2026-03-20 10:32:02 -0700

    Fix Unused Import issue in fbcode/reagent/models/convolutional_network.py
    Differential Revision: D97477512
    
    fbshipit-source-id: dd975d002c485ecba72ddfead062d131ed460aa4

--- reagent/models/convolutional_network.py
+++ reagent/models/convolutional_network.py
@@ -11,7 +11,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.nn.init as init
 from reagent.models.fully_connected_network import FullyConnectedNetwork
 
 
