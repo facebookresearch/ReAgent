4b92f7452c395d4e637d47146fd913c15bab492c
generatedunixname1228101065403401 <noreply+1228101065403401@fb.com>
2026-05-03 06:08:11 -0700

    Fix PYRE_STRICT issue in fbcode/reagent/test/base/test_tensorboardX.py
    Differential Revision: D103586165
    
    fbshipit-source-id: 56b805b70039013cf545f32f7bf9b5dcdc363e33

--- reagent/test/base/test_tensorboardX.py
+++ reagent/test/base/test_tensorboardX.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
 
-# pyre-unsafe
+# pyre-strict
 
 from tempfile import TemporaryDirectory
 from unittest.mock import call, MagicMock
