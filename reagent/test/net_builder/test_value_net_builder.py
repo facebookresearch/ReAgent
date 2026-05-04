8c9d4cd7078a14645b504828d8eedb0924e4df4a
generatedunixname1228101065403401 <noreply+1228101065403401@fb.com>
2026-05-04 04:00:19 -0700

    Fix PYRE_STRICT issue in fbcode/reagent/test/net_builder/test_value_net_builder.py
    Differential Revision: D103651100
    
    fbshipit-source-id: 311d0a5242bdbedfcd9055c3d425cbf72e8586dd

--- reagent/test/net_builder/test_value_net_builder.py
+++ reagent/test/net_builder/test_value_net_builder.py
@@ -15,7 +15,6 @@ from reagent.preprocessing.identify_types import CONTINUOUS
 
 class TestValueNetBuilder(unittest.TestCase):
     def test_fully_connected(self) -> None:
-        # pyre-fixme[28]: Unexpected keyword argument `FullyConnected`.
         chooser = ValueNetBuilder__Union(
             FullyConnected=value.fully_connected.FullyConnected()
         )
