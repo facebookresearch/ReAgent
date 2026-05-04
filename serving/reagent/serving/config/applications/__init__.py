431063481f446a2243ef2a79b4ccf488cff2e77b
generatedunixname1228101065403401 <noreply+1228101065403401@fb.com>
2026-05-04 05:48:46 -0700

    Fix PYRE_STRICT issue in fbcode/reagent/serving/config/applications/__init__.py
    Differential Revision: D103667312
    
    fbshipit-source-id: 54c2aa81d1e158ce9a115bb87b900e929c79d113

--- serving/reagent/serving/config/applications/__init__.py
+++ serving/reagent/serving/config/applications/__init__.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
-# pyre-unsafe
+# pyre-strict
 
 import importlib
 import pkgutil
+import types
 
 
-__all__ = []
+__all__: list[str] = []
 print(list(pkgutil.walk_packages(__path__)))
 
 for _, module_name, _ in pkgutil.walk_packages(__path__):
     __all__.append(module_name)
-    _module = importlib.import_module(f"{__name__}.{module_name}")
+    _module: types.ModuleType = importlib.import_module(f"{__name__}.{module_name}")
     globals()[module_name] = _module
